8e3f44fabeda30c3d5f3e19ad82faae305c7573f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-13 16:43:44 +0400

    update __init__.py

--- AylinRobot/__init__.py
+++ AylinRobot/__init__.py
@@ -1,8 +1,6 @@
-# Leo Projects <https://t.me/leosupportx>
-
 import logging
 from pyrogram import Client
-from config import API_HASH, API_ID, BOT_TOKEN
+from AylinRobot.config import Config
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -10,4 +8,9 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-LeoSongDownloaderBot = Client("LeoSongDownloaderBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+AylinRobot = Client(
+    'AylinRobot',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
