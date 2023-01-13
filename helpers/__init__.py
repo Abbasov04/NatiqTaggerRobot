fc2d465f1b850239bcea5024b9817a05a5520dca
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-13 17:15:00 +0400

    update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.7.0"
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from MusicAzBot.config import Config
+from AylinRobot.config import Config
 
 
 MONGODB_CLI = MongoClient(Config.MONGODB_URI)
