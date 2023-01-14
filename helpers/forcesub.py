1f7c858905445910ba6cb25636ce538329c8ff61
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-14 09:55:54 +0400

    update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -59,5 +59,5 @@ async def ForceSub(bot: Client, event: Message):
         fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
-        print(f"Nə isə səhv getdi!  Məcburi Abunə olmaq mümkün deyil.\nXəta: {err}\n\nƏyər Bu Xətanı Yenə Alırsınızsa Sahib .:)",
+        print(f"Nə isə səhv getdi!  Məcburi Abunə olmaq mümkün deyil.\nXəta: {err}\n\nƏyər Bu Xətanı Yenə Alırsınızsa")
         return 200
