1c508e35b75be924355465daba5fa8d4b10c52d5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-15 01:25:41 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -21,6 +21,7 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from AylinRobot.Plugin import *
 from AylinRobot.Music import *
+from AylinRobot.Oyunlar import *
 from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AylinRobot import AylinRobot as app
