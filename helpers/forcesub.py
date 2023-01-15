a0c747d464825c60cf4d3d30dba4ab557f9cd26e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-15 17:05:01 +0400

    update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -36,10 +36,10 @@ async def ForceSub(bot: Client, event: Message):
             return 400
         else:
             return 200
+          await message.reply_photo(
+        AylinIMG,            
     except UserNotParticipant:
         await bot.send_message(
-          await message.reply_photo(
-        AylinIMG,
             chat_id=event.from_user.id,
             text="<b>{} Salam</b> {},\n\n<b>Sən hələ də Playlist Kanalıma Qoşulmamısan ☹️ \nPlaylist Kanalıma, Qoşulun Və 'Yenilə 🔄' Düyməsinə Toxun</b>".format(event.from_user.mention),
             reply_markup=InlineKeyboardMarkup(
