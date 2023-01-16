b8b8f3e52cca07f40720ed4518773a466df5f048
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-17 01:41:56 +0400

    update hack.py

--- AylinRobot/Plugin/hack.py
+++ AylinRobot/Plugin/hack.py
@@ -1,1 +1,24 @@
 
+import logging
+from tglogging import TelegramLogHandler
+
+# TGLOGGING Uygulamanızın logunu Telegram'a anlık göndermenizi sağlar. 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token="5636190781:AAGDOmFJmYDaJcPuW1lWyEAMgN-SwyycvRA", 
+            log_chat_id="-1001805104889", 
+            update_interval=2, 
+            minimum_lines=3, # Her Mesajda gönderilecek satır sayısı
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("pyrogram - telethon")
+
+logger.info("Telegram'a canlı log başlatıldı.")
