c70c7e62f2b2114253f234317864aafdc9fbe6ad
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-17 01:39:45 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -62,29 +62,7 @@ async def show_status_count(_, client: Message):
         quote=True
     )
 
-
-import logging
-from tglogging import TelegramLogHandler
-
-# TGLOGGING Uygulamanızın logunu Telegram'a anlık göndermenizi sağlar. 
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        TelegramLogHandler(
-            token="5636190781:AAGDOmFJmYDaJcPuW1lWyEAMgN-SwyycvRA", 
-            log_chat_id="-1001805104889", 
-            update_interval=2, 
-            minimum_lines=1, # Her Mesajda gönderilecek satır sayısı
-            pending_logs=200000),
-        logging.StreamHandler()
-    ]
-)
-
-logger = logging.getLogger("pyrogram - telethon")
-
-logger.info("Telegram'a canlı log başlatıldı.")
+app.start()
+LOGGER.info(f"{Config.BOT_USERNAME} Uğurla Başladı Sahibim {Config.OWNER_NAME}")
 
 idle()
