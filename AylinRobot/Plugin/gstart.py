11f27300658a571f98c9f353d5d927ec10f7690c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-21 00:19:07 +0400

    update gstart.py

---
+++ AylinRobot/Plugin/gstart.py
@@ -0,0 +1,7 @@
+from AylinRobot.config import Config
+from pyrogram.types import Message
+from AylinRobot import AylinRobot as app
+from pyrogram import idle, filters
+from AylinRobot.config import Config
+from pyrogram import Client, filters
+
