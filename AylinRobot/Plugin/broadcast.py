4cb4c061867129e3a6f240ecd0c83d3a38f3e3bf
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-20 12:44:42 +0400

    update broadcast.py

--- AylinRobot/Plugin/broadcast.py
+++ AylinRobot/Plugin/broadcast.py
@@ -516,7 +516,7 @@ def humanbytes(size):
 ########### ÇOKLU DİL ##############
 class LAN(object):
 
-elif Config.LANGAUGE == "AZ":
+elif Config.LANGAUGE == "AZ"
 
         BILDIRIM = "```📣 Yeni İsmarıc``` \n\n#YENI_ISTIFADƏÇİ **botu başlatdı!** \n\n🏷 isim: `{}` \n📮 istifadəçi id: `{}` \n🧝🏻‍♂️ profil linki: [{}](tg://user?id={})"
         GRUP_BILDIRIM = "```📣 Yeni İsmarıc``` \n\n#YENI_QRUP **botu başlatdı!** \n\n🏷 Qrupa əlavə edən: `{}` \n📮 Qrupa əlavə edən istifadəçi id: `{}` \n🧝🏻‍♂️ profil linki: [{}](tg://user?id={})\n Qrupun adı: {}\n Qrupun ID: {}\n Qrupun mesaj kinki( sadəcə açıq qruplar): [Buraya Toxun](https://t.me/c/{}/{})"
