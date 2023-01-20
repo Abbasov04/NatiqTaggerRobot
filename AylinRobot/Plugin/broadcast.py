78decc13621280cb0121044b01058b9f4da927b8
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-20 10:15:46 +0400

    update broadcast.py

--- AylinRobot/Plugin/broadcast.py
+++ AylinRobot/Plugin/broadcast.py
@@ -545,7 +545,7 @@ class LAN(object):
         BLOCKS = "🆔 **Kullanıcı ID**: `{}`\n⏱ **Süre**: `{}`\n🗓 **Yasaklanan Tarih**: `{}`\n💬 **Sebep**: `{}`\n\n"
         TOTAL_BLOCK = "🚷 **Toplam Yasaklanan:** `{}`\n\n{}"
 
-    elif LANGAUGE == "AZ":
+    elif Config.LANGAUGE == "AZ":
 
         BILDIRIM = "```📣 Yeni İsmarıc``` \n\n#YENI_ISTIFADƏÇİ **botu başlatdı!** \n\n🏷 isim: `{}` \n📮 istifadəçi id: `{}` \n🧝🏻‍♂️ profil linki: [{}](tg://user?id={})"
         GRUP_BILDIRIM = "```📣 Yeni İsmarıc``` \n\n#YENI_QRUP **botu başlatdı!** \n\n🏷 Qrupa əlavə edən: `{}` \n📮 Qrupa əlavə edən istifadəçi id: `{}` \n🧝🏻‍♂️ profil linki: [{}](tg://user?id={})\n Qrupun adı: {}\n Qrupun ID: {}\n Qrupun mesaj kinki( sadəcə açıq qruplar): [Buraya Toxun](https://t.me/c/{}/{})"
