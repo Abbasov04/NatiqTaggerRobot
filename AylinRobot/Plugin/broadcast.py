1f395d62a3f93c6fe8f43a5b48ad72bafd0ed5a2
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-20 09:56:24 +0400

    update broadcast.py

--- AylinRobot/Plugin/broadcast.py
+++ AylinRobot/Plugin/broadcast.py
@@ -370,7 +370,7 @@ async def delcmd_off(chat_id: int): # Grup için mesaj silme özeliğini kapatı
 # Verileri listeleme komutu
 @app.on_message(filters.command("stats") & filters.user(Config.OWNER_ID))
 async def botstats(app: Client, message: Message):
-    g4rip = await app.send_message(message.chat.id, LAN.STATS_STARTED.format(message.from_user.mention))
+    g4rip = await app.send_message(message.chat.id, AylinRobot.Plugin.LAN.STATS_STARTED.format(message.from_user.mention))
     all_users = await db.get_all_users()
     groups = 0
     pms = 0
