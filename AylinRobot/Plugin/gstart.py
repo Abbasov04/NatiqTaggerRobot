734fde84aa6b0e26704602ec0ec775b1e6264af0
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-21 00:21:52 +0400

    update gstart.py

--- AylinRobot/Plugin/gstart.py
+++ AylinRobot/Plugin/gstart.py
@@ -5,3 +5,26 @@ from pyrogram import idle, filters
 from AylinRobot.config import Config
 from pyrogram import Client, filters
 
+import logging
+from tglogging import TelegramLogHandler
+
+# TGLOGGING Uygulamanızın logunu Telegram'a anlık göndermenizi sağlar. 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token=f"{Config.BOT_TIKEN}", 
+            log_chat_id=f"{Config.LOG_GROUP}", 
+            update_interval=2, 
+            minimum_lines=1, # Her Mesajda gönderilecek satır sayısı
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("pyrogram - telethon")
+
+logger.info("Telegram'a canlı log başlatıldı.")
