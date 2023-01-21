857e47402b79c2edceb5d68db9674c0693816b1c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-21 15:27:30 +0400

    update carbon.py

--- AylinRobot/Plugin/carbon.py
+++ AylinRobot/Plugin/carbon.py
@@ -34,7 +34,7 @@ async def make_carbon(code):
 @app.on_message(filters.command("carbon"))
 async def carbon_func(bot: app, msg: Message):
     m = await msg.reply_text("Dəstəklənən media faylına cavab verin")  
-    await msg.edit("`Hazırlanır`")
+    await msg.m.edit("`Hazırlanır`")
     carbon = await make_carbon(msg.reply_to_message.text)
     await m.edit("`Göndərilir`")
     await bot.send_photo(msg.chat.id, photo=carbon)
