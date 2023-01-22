3e4189163ad40a9c8b21581b1d169c03034ffdf7
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-22 12:00:11 +0400

    update gstart.py

---
+++ AylinRobot/Plugin/gstart.py
@@ -0,0 +1,77 @@
+from AylinRobot.config import Config
+from AylinRobot.translation import Translation    
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from AylinRobot import AylinRobot as app
+from AylinRobot import LOGGER    
+    
+    
+    
+    
+    
+    
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton("👾 Botlist", url=f"https://t.me/{Config.BOTLIST}"),
+        InlineKeyboardButton("📢 Kanal", url=f"https://t.me/{Config.SUPPORT}"),
+        ],[                  
+        InlineKeyboardButton('💠 Kömək', callback_data='help'),
+        ],[        
+        InlineKeyboardButton('➕ Məni Qrupa Əlavə Et ➕', url=f"https://t.me/{Config.BOT_USERNAME}?startgroup=true"),
+        ],[                
+        InlineKeyboardButton('Sahibim🧑‍💻',  url=f"https://t.me/{Config.OWNER_NAME}"),
+        InlineKeyboardButton("🎵 Playlist", url=f"https://t.me/{Config.PLAYLIST_NAME}"),
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🎵 Musiqi', callback_data='musıc'),
+        InlineKeyboardButton('⭐ Telegraph', callback_data='tg')
+        ],[
+        InlineKeyboardButton('🎮 Oyunlar', callback_data='oyun'),
+        InlineKeyboardButton('🇦🇿 Şəhidlər', callback_data='sehıd'),
+        ],[        
+        InlineKeyboardButton('🌀 Əyləncə', callback_data='eylence'),
+        InlineKeyboardButton('Əlavələr', callback_data='elave'),
+        ],[
+        InlineKeyboardButton('👨‍💻 Sahib Əmrləri', callback_data='sahib'), 
+        ],[        
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='home'),
+        ]]
+    )
+    
+    
+    MUSIC_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )
+    TELEGRAPH_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )
+    SEHID_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )        
+    OYUN_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )
+    EYLENCE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )     
+    SAHIB_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    )
+    ELAVE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),
+        ]]
+    ) 
