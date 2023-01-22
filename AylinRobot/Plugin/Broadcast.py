b42384fa917ff0e6a8ffea2f08d168c29685dd5a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-22 18:38:15 +0400

    update Broadcast.py

--- AylinRobot/Plugin/Broadcast.py
+++ AylinRobot/Plugin/Broadcast.py
@@ -2,7 +2,6 @@ import shutil, psutil, traceback, os, datetime, random, string, time, traceback,
 from AylinRobot.translation import *
 from AylinRobot.config import Config
 from AylinRobot import AylinRobot as app
-from AylinRobot.Plugin import Bloklama
 from helpers.filters import command
 from pyrogram import Client as USER
 from helpers.chats import add_served_chat, blacklisted_chats, get_served_chats
@@ -363,6 +362,101 @@ async def G4RIP(app: Client, cmd: Message):
 @app.on_message(filters.command("broadcastall") & filters.user(Config.OWNER_ID) & filters.reply)
 async def broadcast_handler_open(_, m: Message):
     await main_broadcast_handler(m, db)
+
+
+
+# Bir kullanıcı yasaklama komutu
+@app.on_message(filters.command("block") & filters.user(Config.OWNER_ID))
+async def ban(c: Client, m: Message):
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+        if len(m.command) <= 1:
+            ban_duration = 9999
+            ban_reason = LAN.BAN_REASON.format(Config.BOT_USERNAME)
+        elif len(m.command) == 2:
+            ban_duration = 9999
+            ban_reason = " ".join(m.command[1:])
+    else:
+        if len(m.command) <= 1:
+            return await m.reply(LAN.NEED_USER)
+        elif len(m.command) == 2:
+            user_id = int(m.command[1])
+            ban_duration = 9999
+            ban_reason = LAN.BAN_REASON.format(Config.BOT_USERNAME)
+        elif len(m.command) == 3:
+            user_id = int(m.command[1])
+            ban_duration = 9999
+            ban_reason = " ".join(m.command[2:])
+    
+        if str(user_id).startswith("-"):
+            try:    
+                ban_log_text = LAN.BANNED_GROUP.format(m.from_user.mention, user_id, ban_duration, ban_reason)
+                await c.send_message(user_id, LAN.AFTER_BAN_GROUP.format(ban_reason))
+                await c.leave_chat(user_id)
+                ban_log_text += LAN.GROUP_BILGILENDIRILDI
+            except BaseException:
+                traceback.print_exc()
+                ban_log_text += LAN.GRUP_BILGILENDIRILEMEDI.format(traceback.format_exc())
+        else:
+            try:    
+                ban_log_text = LAN.USER_BANNED.format(m.from_user.mention, user_id, ban_duration, ban_reason)
+                await c.send_message(user_id, LAN.AFTER_BAN_USER.format(ban_reason))
+                ban_log_text += LAN.KULLANICI_BILGILENDIRME
+            except BaseException:
+                traceback.print_exc()
+                ban_log_text += LAN.KULLANICI_BILGILENDIRMEME.format(traceback.format_exc())
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        await c.send_message(Config.LOG_CHANNEL, ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+
+
+
+# Bir kullanıcın yasağını kaldırmak komutu
+@app.on_message(filters.command("unblock") & filters.user(Config.OWNER_ID))
+async def unban(c: Client, m: Message):
+        if m.reply_to_message:
+            user_id = m.reply_to_message.from_user.id
+        else:
+            if len(m.command) <= 1:
+                return await m.reply(LAN.NEED_USER)
+            else:
+                user_id = int(m.command[1])
+        unban_log_text = LAN.UNBANNED_USER.format(m.from_user.mention, user_id)
+        if not str(user_id).startswith("-"):
+            try:
+                await c.send_message(user_id, LAN.USER_UNBAN_NOTIFY)
+                unban_log_text += LAN.KULLANICI_BILGILENDIRME
+            except BaseException:
+                traceback.print_exc()
+                unban_log_text += LAN.KULLANICI_BILGILENDIRMEME.format(traceback.format_exc())
+        await db.remove_ban(user_id)
+        await c.send_message(Config.LOG_CHANNEL, unban_log_text)
+        await m.reply_text(unban_log_text, quote=True)
+
+
+
+# Yasaklı listesini görme komutu
+@app.on_message(filters.command("blocklist") & filters.user(Config.OWNER_ID))
+async def _banned_usrs(_, m: Message):
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ""
+    async for banned_user in all_banned_users:
+        user_id = banned_user["id"]
+        ban_duration = banned_user["ban_status"]["ban_duration"]
+        banned_on = banned_user["ban_status"]["banned_on"]
+        ban_reason = banned_user["ban_status"]["ban_reason"]
+        banned_usr_count += 1
+        text += LAN.BLOCKS.format(user_id, ban_duration, banned_on, ban_reason)
+    reply_text = LAN.TOTAL_BLOCK.format(banned_usr_count, text)
+    if len(reply_text) > 4096:
+        with open("banned-user-list.txt", "w") as f:
+            f.write(reply_text)
+        await m.reply_document("banned-user-list.txt", True)
+        os.remove("banned-user-list.txt")
+        return
+    await m.reply_text(reply_text, True)
+
 
 
 ############## BELİRLİ GEREKLİ DEF'LER ###########
