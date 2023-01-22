5bdd2122cec4df09764fcce361e5f264f711aec8
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-22 16:20:03 +0400

    update broadcast.py

--- AylinRobot/Plugin/broadcast.py
+++ AylinRobot/Plugin/broadcast.py
@@ -1,5 +1,6 @@
 import shutil, psutil, traceback, os, datetime, random, string, time, traceback, aiofiles, asyncio
 from AylinRobot.translation import Translation
+class LAN(object):
    if Config.LANGAUGE == "AZ":
 from AylinRobot.config import Config
 from AylinRobot import AylinRobot as app
