3936946d9be360aa47dec0bf0c908a996b4da9ee
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-22 12:25:04 +0400

    update cb_buttons.py

--- AylinRobot/Plugin/cb_buttons.py
+++ AylinRobot/Plugin/cb_buttons.py
@@ -4,7 +4,7 @@
 
 
 import os
-from AylinRobot.Plugin import Translation
+from AylinRobot.Plugin import *
 from AylinRobot.config import Config
 from pyrogram import Client
 from pyrogram.errors import UserNotParticipant
