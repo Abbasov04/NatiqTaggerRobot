4081792f5cfcd2fa30c5fc6e2398b008162b83a7
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-22 16:27:03 +0400

    update translation.py

--- AylinRobot/translation.py
+++ AylinRobot/translation.py
@@ -1,7 +1,6 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AylinRobot.config import Config
 class Translation(object):
-    if Config.LANGAUGE == "AZ":
     START_TEXT = """
 **Salam {} 👋**
 
