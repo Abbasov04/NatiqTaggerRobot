d26a63b1e672ffe8cc8323319afa0a853f338c3f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-23 13:20:07 +0400

    update access_db.py

--- helpers/database/access_db.py
+++ helpers/database/access_db.py
@@ -4,6 +4,7 @@
 
 from AylinRobot.config import Config
 from helpers.database.database import Database
+from AylinRobot.Plugin import Broadcast
 
 db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
 mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
