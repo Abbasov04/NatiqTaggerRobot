282a319d6e1f71606cf9304d57ca2f5f1c35ab3f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-23 15:26:03 +0400

    update access_db.py

--- helpers/database/access_db.py
+++ helpers/database/access_db.py
@@ -2,7 +2,7 @@
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
-from MusicAzBot.config import Config
+from AylinRobot.config import Config
 from helpers.database.database import Database
 
 db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
