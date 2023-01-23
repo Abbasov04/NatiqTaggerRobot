2fee14729e7df5cc7f39ec0b9cf4d3599570602c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-23 13:31:44 +0400

    update Broadcast.py

--- AylinRobot/Plugin/Broadcast.py
+++ AylinRobot/Plugin/Broadcast.py
@@ -2,8 +2,6 @@ import shutil, psutil, traceback, os, datetime, random, string, time, traceback,
 from AylinRobot.translation import *
 from AylinRobot.config import Config
 from AylinRobot import AylinRobot as app
-from helpers.database.access_db import db
-from helpers.broadcast import broadcast_handler
 from helpers.filters import command
 from pyrogram import Client as USER
 from helpers.chats import add_served_chat, blacklisted_chats, get_served_chats
@@ -21,13 +19,6 @@ from pyrogram.errors import (
 
 
 chat_watcher_group = 10
-
-
-
-@app.on_message(filters.private & filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply)
-async def _broadcast(_, client: Message):
-    await broadcast_handler(client)
-
 
 
 @app.on_message(group=chat_watcher_group)
