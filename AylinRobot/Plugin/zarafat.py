960b47891dc8de930302d00e5bf780c7c184ad3a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-24 13:38:29 +0400

    update zarafat.py

--- AylinRobot/Plugin/zarafat.py
+++ AylinRobot/Plugin/zarafat.py
@@ -2,7 +2,6 @@
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
-import time
 from asyncio import sleep
 from time import time
 from random import choice
@@ -12,47 +11,14 @@ from pyrogram import idle, filters
 from AylinRobot.config import Config
 from pyrogram import Client, filters
 
-@app.on_message(filters.command("zarafat") & ~filters.edited)
-async def police(app: Client, msg: Message):
-    m = await msg.reply("⚠️⚠️⚠️Zarafat Şoww😨😨😨😨😱😱😱😱😱 \n")
-    await sleep(3)
-    await m.edit("😱😱⚠️⚠️ 😂😂😂😂😂😂😂😂😂😂😂😂😂😂😱😵 \n")
-    await sleep(3)
-    await m.edit("😂😂👍👍👍👍👍👍👍👍👍👍👍👍👍 Zarafat \n")
-    await sleep(3)
-    await m.edit("Zor zarafat idi ahahahah \n")
-    await sleep(3)
-    await m.edit("AHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHA \n")
-    await sleep (3)
-    await m.edit("HAHAHAHAHAHAHHAHAHAHAHAHAHA😂😂😂😂😂😂😂😂 \n")
-    await sleep (3)
-    await m.edit("😂 ALA ÖLDÜM GÜLMƏKDƏN \n")    
-    await sleep (3)
-    await m.edit("hHALALDI SƏMƏ ✔️✔️✔️✔️✔️✔️✔️✔️👏👏👏👏👏👏👏👏 \n")
-    await sleep (3)
-    await m.edit("👏 BOMBA ZARAFAT👏👏👏👏👏😂😂😂😂 \n")
-    await sleep (3)
-    await m.edit("😂😂😂😂😂😂⚠️ \n")   
-    await sleep (3)
-    await m.edit("💯💯💯💯💯💯💯💯💯 \n")        
-    await sleep (3)
-    await m.edit("ALA KOPYA BİZİYE 😂😂😂👏👏 \n")
-    await sleep (3)
-    await m.edit("💯💯⚠️⚠️♿️YOL POSTUNUN SAHİBİNİ VƏ MÜDAFİƏÇİLƏRİNİ AÇIN \n")
-    await sleep (3)
-    await m.edit("GƏLİR ♿️♿️ DUTT️\n ")                
-    await sleep (3)
-    await m.edit("DÜÜÜÜT♿️DÜÜT♿️💯💯⚠️ \n")                
-    await sleep (3)
-    await m.edit("♿️ALALAALALALALA ♿️ \n")                
-    await sleep (3)
-    await m.edit("CJWJCJWJXJJWDJJQUXJAJXJAJXJWJFJWJXJAJXJWJXJWJFIWIXJQJJQJASJAXJ \n") 
-    await sleep (3)
-    await m.edit("AJXJAJXJJAJXJWJFWJJFWIIFIWICIWIFIWICJAXJWJFJEICIIEICIEIFIWICJSXJJS \n")           
-    await sleep (3)
-    await m.edit("CJEIVIAJXBWJCJIQICIWJX💯💯💯💯💯💯😂😂😂😂😂😂😂 \n")           
-    await sleep (3)
-    await m.edit("😂⚠️😂😂😂😂😂😂⚠️⚠️⚠️😂😂😂😂♿️♿️♿️😅😅 \n")
-    await sleep (3)
-    await m.edit("😅😂👏💯⚠️👏♿️🚨")     
+@app.on_message(command(["reload"]))
+@authorized_users_only
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await client.send_message(message.chat.id, "✅ Bot **reloaded correctly!**\n\n• The **Admin list** has been **updated.**")
 
