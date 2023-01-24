750347ef25172664cf88d88b8150954ff40671d4
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-24 17:16:00 +0400

    update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -21,5 +21,4 @@ class Config:
    SUPPORT = os.environ.get("SUPPORT", "HuseynH")
    ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/b2c2ed59a89933a384ae3.jpg")
-   LOG_GROUP = int(os.environ.get("LOG_GROUP", "-1001838340601"))   
    
