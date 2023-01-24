4fb59408b407625192f8cda9972707669af4d9e1
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-24 15:29:56 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -11,13 +11,14 @@ import asyncio
 from AylinRobot.config import Config
 from asyncio import TimeoutError
 from AylinRobot.translation import Translation
-from AylinRobot.Plugin import Button
-from helpers.database.access_db import db
-from helpers.database.add_user import AddUserToDatabase
+from helper.database.access_db import db
+from helper.broadcast import broadcast_handler
+from helper.database.add_user import AddUserToDatabase
+from helper.display_progress import humanbytes
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
-from AylinRobot.Plugin import *
+from AylinRobot.Plugins import *
 from AylinRobot.Music import *
 from AylinRobot.Oyunlar import *
 from pyrogram import idle, filters
@@ -25,17 +26,24 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AylinRobot import AylinRobot as app
 from AylinRobot import LOGGER
 
-AylinIMG = f"{Config.START_IMG}"
+AylinRobotIMG = f"{Config.START_IMG}"
+
+
 
 @app.on_message(filters.private & filters.incoming & filters.command(['start']))
 async def start(client, message):
     await AddUserToDatabase(client, message)
     await message.reply_photo(
-        AylinIMG,
+        AylinRobotIMG,
         caption=Translation.START_TEXT.format(message.from_user.mention, Config.BOT_USERNAME),
-        reply_markup=Button.START_BUTTONS
+        reply_markup=Translation.START_BUTTONS
     )
     
+    
+@app.on_message(filters.private & filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply)
+async def _broadcast(_, client: Message):
+    await broadcast_handler(client)
+
 
 @app.on_message(filters.new_chat_members)
 async def new_chat(c: Client, m: Message):
@@ -59,6 +67,23 @@ async def new_chat(c: Client, m: Message):
 
 
 
+
+
+@app.on_message(filters.private & filters.command("status") & filters.user(Config.OWNER_ID))
+async def show_status_count(_, client: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await client.reply_text(
+        text=f"**Ümumi Disk Sahəsi:** {total} \n**İstifadə edilmiş Sahə:** {used}({disk_usage}%) \n**Boş Yer:** {free} \n**CPU  İstifadə:** {cpu_usage}% \n**RAM İstifadəsi:** {ram_usage}%\n\n**{Config.BOT_USERNAME}-Ümumi İstifadəçiləri:** `{total_users}`"  ,
+        parse_mode="Markdown",
+        quote=True
+    )
 
 app.start()
 LOGGER.info(f"{Config.BOT_USERNAME} Uğurla Başladı Sahibim {Config.OWNER_NAME}")
