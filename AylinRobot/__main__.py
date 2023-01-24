cc527f8902aa6a16561f5248de911855214cc042
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-24 17:45:12 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -11,7 +11,6 @@ import asyncio
 from AylinRobot.config import Config
 from asyncio import TimeoutError
 from AylinRobot.translation import Translation
-from helpers.database.access_db import db
 from helpers.database.add_user import AddUserToDatabase
 from helpers.display_progress import humanbytes
 from pyrogram import Client
