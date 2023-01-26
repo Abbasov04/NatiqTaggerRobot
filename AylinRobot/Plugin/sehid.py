b301ccc1deb5786e61f56e79bcf796273cdc9c12
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-26 23:43:10 +0400

    update sehid.py

--- AylinRobot/Plugin/sehid.py
+++ AylinRobot/Plugin/sehid.py
@@ -7,6 +7,38 @@ from pyrogram import filters
 from Sehid import random_line
 
 
-@app.on_message(filters.command("sehid") & ~filters.edited)
-async def commit(_, message):
-    await message.reply_text((await random_line('Sehid/sehid.txt')))
+
+test = open("Sehid/sehid.txt")
+yazilar = test.readlines()
+rndyazi = random.choice(yazilar)
+
+@app.on_message(filters.command('ayuye'))
+async def start(client, msj):
+    global soz
+    chat_id = msj.chat.id
+    soz = random.choice(sozler)
+    await client.send_message(chat_id,
+                              soz,
+                              reply_markup=InlineKeyboardMarkup(
+                                  [
+                                      [
+                                          InlineKeyboardButton(
+                                              "Deyisdir",
+                                              callback_data="deyisdir"
+                                          )
+                                      ]
+                                  ]
+                              )
+                              )
+
+@app.on_callback_query(filters.regex(r"^deyisdir$"))
+async def callback(cl, msj: CallbackQuery):
+    soz2 = random.choice(sozler)
+    if soz2 == soz:
+        soz2 = random.choice(sozler)
+        if soz2 == soz:
+            pass
+        elif soz2 != soz:
+            await msj.message.edit_text(text=soz2, reply_markup=msj.message.reply_markup)
+    elif soz2 != soz:
+        await msj.message.edit_text(text=soz2, reply_markup=msj.message.reply_markup)
