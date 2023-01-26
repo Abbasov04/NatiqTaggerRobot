c7975a2d1ee3d279b60b9070ede3e17e69dc26d8
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-26 18:32:53 +0400

    update sehid.py

--- AylinRobot/Plugin/sehid.py
+++ AylinRobot/Plugin/sehid.py
@@ -7,6 +7,18 @@ from pyrogram import filters
 from Sehid import random_line
 
 
-@app.on_message(filters.command("sehid") & ~filters.edited)
+@app.on_callback_query(filters.regex("sehid"))
 async def commit(_, message):
     await message.reply_text((await random_line('Sehid/sehid.txt')))
+    
+    
+    
+    
+@app.on_message(command("sehid") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>✨ Welcome {message.from_user.mention()}!</ballows you to play music on groups through the new Telegram's voice chats!**
+
+💡 Find out all the **Bot's commands** and how they work by clicking on the **» ⚙️ Commands** button!""",
+     disable_web_page_preview=True
+    )    
