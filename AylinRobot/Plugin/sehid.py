53dddbe98dd548d3b854376de128799e502edea2
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-26 18:44:16 +0400

    update sehid.py

--- AylinRobot/Plugin/sehid.py
+++ AylinRobot/Plugin/sehid.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from helpers.filters import command
 
 
-@app.on_callback_query(filters.regex("sehid"))
+@app.on_callback_query(filters.regex("sehidler"))
 async def commit(_, message):
     await message.reply_text((await random_line('Sehid/sehid.txt')))
     
@@ -22,5 +22,14 @@ async def start_(client: Client, message: Message):
         f"""<b>✨ Welcome {message.from_user.mention()}!</ballows you to play music on groups through the new Telegram's voice chats!**
 
 💡 Find out all the **Bot's commands** and how they work by clicking on the **» ⚙️ Commands** button!""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "➕ Add me to your group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ],[
+                    InlineKeyboardButton(
+                        "⚙️ Command​​", callback_data="sehidler"
+                    ),
      disable_web_page_preview=True
     )    
