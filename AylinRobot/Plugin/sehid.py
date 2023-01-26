5343f44fc6b8d7cade3bc3cf6a6d643b39be2521
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-27 00:09:37 +0400

    update sehid.py

--- AylinRobot/Plugin/sehid.py
+++ AylinRobot/Plugin/sehid.py
@@ -13,7 +13,7 @@ from Sehid import random_line
 
 sehid = open("Sehid/sehid.txt")
 sehid = sehid.readlines()
-rndyazi = random.choice(sehid)
+sehid = random.choice(sehid)
 
 @app.on_message(filters.command('sehid'))
 async def start(client, msj):
