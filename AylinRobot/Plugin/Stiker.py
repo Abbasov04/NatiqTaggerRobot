f693bf0a35858d6fb2d74c3b9bb20fe88507d5ec
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-28 13:21:21 +0400

    update Stiker.py

--- AylinRobot/Plugin/Stiker.py
+++ AylinRobot/Plugin/Stiker.py
@@ -11,44 +11,44 @@ import aiohttp
 from pyrogram import Client, filters
 
 
-@app.on_message(filters.command(["git"]))
-@capture_err
-async def github(_, message):
-    if len(message.command) != 2:
-        await message.reply_text("🙄__Give me a valid github username__")
-        return
-    username = message.text.split(None, 1)[1]
-    URL = f"https://api.github.com/users/{username}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL) as request:
-            if request.status == 404:
-                return await message.reply_text("404")
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
 
-            result = await request.json()
-            try:
-                url = result["html_url"]
-                name = result["name"]
-                company = result["company"]
-                bio = result["bio"]
-                created_at = result["created_at"]
-                avatar_url = result["avatar_url"]
-                blog = result["blog"]
-                location = result["location"]
-                repositories = result["public_repos"]
-                followers = result["followers"]
-                following = result["following"]
-                caption = f"""**Info Of {name}**
-**🧸Username:** `{username}`
-**💌Bio:** `{bio}`
-**🔗Profile Link:** [Here]({url})
-**⛱Company:** `{company}`
-**🗓Created On:** `{created_at}`
-**🧰Repositories:** `{repositories}`
-**🎓Blog:** `{blog}`
-**📍Location:** `{location}`
-**💡Followers:** `{followers}`
-**📡Following:** `{following}`"""
-            except Exception as e:
-                print(str(e))
-    await message.reply_photo(photo=avatar_url, caption=caption)
+from googletrans import Translator
+from gtts import gTTS
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR
+
+
+def convert(text):
+    audio = BytesIO()
+    i = Translator().translate(text, dest="en")
+    lang = i.src
+    tts = gTTS(text, lang=lang)
+    audio.name = lang + ".ogg"
+    tts.write_to_fp(audio)
+    return audio
+
+
+@app.on_message(filters.command(["tts"]))
+async def text_to_speech(_, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("__😫Reply to some texts!__")
+    if not message.reply_to_message.text:
+        return await message.reply_text("__❗️Reply to some texts!__")
+    m = await message.reply_text("🔁 Processing...")
+    text = message.reply_to_message.text
+    try:
+        loop = get_running_loop()
+        audio = await loop.run_in_executor(None, convert, text)
+        await message.reply_audio(audio)
+        await m.delete()
+        audio.close()
+    except Exception as e:
+        await m.edit(str(e))
+        es = traceback.format_exc()
+        print(es)
 
