73a6b743a654ee28535a2396e722d9466f77e816
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-29 10:36:26 +0400

    update Ship.py

--- AylinRobot/Plugin/Ship.py
+++ AylinRobot/Plugin/Ship.py
@@ -15,6 +15,7 @@ async def my_handler(client, msj):
     chat_id = msj.chat.id
     chat_members = app.get_chat_members(chat_id)
     BU_QRUP_USERLERI = []
+    for member in chat_members:
         if member.user.is_bot == True:
             pass
         elif member.user.is_bot == False:
