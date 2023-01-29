a2c08a11eebda9188f9000b2a032f0fed28571e3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-29 18:01:17 +0400

    update Ship.py

--- AylinRobot/Plugin/Ship.py
+++ AylinRobot/Plugin/Ship.py
@@ -19,7 +19,7 @@ import random
 async def start(client, msj):
     BOTLAR = []
     chat_id = msj.chat.id
-    async for members in app.get_chat_members(chat_id):
+    async for members in client.get_chat_members(chat_id):
         botmu = members.user.is_bot
         if botmu == True:
             BOTLAR.append(members.user.mention)
