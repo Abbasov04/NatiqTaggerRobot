dc6f141d524d443cae197de342a55f8b28278cb6
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-29 18:46:26 +0400

    update Botlist.py

--- AylinRobot/Plugin/Botlist.py
+++ AylinRobot/Plugin/Botlist.py
@@ -2,15 +2,15 @@ from pyrogram.types import Message
 from AylinRobot import AylinRobot as app
 from pyrogram import idle, filters
 from pyrogram import Client, filters
-import asyncio
-from pyrogram import Client
+
+
 
     
-@app.on_message(filters.command('list') & filters.group))
-def start(app, msj):
+@app.on_message(filters.command('list') & filters.group)
+def start(client, msj):
     BOTLAR = []
     chat_id = msj.chat.id
-    async for members in app.get_chat_members(chat_id):
+    async for members in client.get_chat_members(chat_id):
         botmu = members.user.is_bot
         if botmu == True:
             BOTLAR.append(members.user.mention)
@@ -18,4 +18,4 @@ def start(app, msj):
         else:
             pass
     botlarsiyahi = '\n'.join(BOTLAR)
-    await app.send_message(chat_id, botlarsiyahi)
+    await client.send_message(chat_id, botlarsiyahi)
