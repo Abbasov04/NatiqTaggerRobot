431cfef6be121736656782340df1e4ef27845d00
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-30 22:54:24 +0400

    update Botlist.py

--- AylinRobot/Plugin/Botlist.py
+++ AylinRobot/Plugin/Botlist.py
@@ -2,6 +2,8 @@ from pyrogram.types import Message
 from AylinRobot import AylinRobot as app
 from pyrogram import idle, filters
 from pyrogram import Client, filters
+import aiohttp
+from pyrogram import Client, filters
 
 
 
