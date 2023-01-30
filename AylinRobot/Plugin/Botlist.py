5bac2190c27a77cd1f724111acb215289a431896
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-30 16:34:41 +0400

    update Botlist.py

--- AylinRobot/Plugin/Botlist.py
+++ AylinRobot/Plugin/Botlist.py
@@ -5,39 +5,3 @@ from pyrogram import Client, filters
 
 
 
-    
-@app.on_message(filters.command('github')) async def github (_, message):
-if len(message.command) != 2:
-await message.reply_text("/github İstifadəçi adı yazmadınız ")
-return
-username = message.text.split(None, 1) [1] URL f'https://api.github.com/users/{username}'
-async with aiohttp.ClientSession() as session: async with session.get(URL) as request:
-if request.status == 404:
-return await message.reply_text("404")
-result = await request.json()
-try:
-url = result['html_url']
-name = result['name']
-company result['company']
-bio = result['bio']
-created_at = result['created_at']
-avatar_url = result['avatar_url']
-blog = result['blog']
-location = result['location']
-repositories = result['public_repos']
-followers = result['followers']
-following result[ following"] caption = """**Info Of {name}****
-İstifadəçi adı: *** [username}
-** Bio: ** (bio)
-***Profil linki: **[Here]({url})**
-Şirkət: ** {company}
-**Yaradılma tarixi: ** {created_at}
-***Depolar: **`{repositories}
-***Blog: ** (blog)
-***Məkan: *** {location}
-**Dİzləyicilər: ***{followers} **Izlədikləri: ** (following}
-""""
-except Exception as e:
-print(str(e))
-
-pass await message.reply_photo(photo-avatar_url, caption-caption)
