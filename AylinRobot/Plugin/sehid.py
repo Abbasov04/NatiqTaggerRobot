bc493e453caa34b7c072eb3beb673a8fcc9052e1
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-01-30 23:07:16 +0400

    update sehid.py

--- AylinRobot/Plugin/sehid.py
+++ AylinRobot/Plugin/sehid.py
@@ -2,11 +2,22 @@ import secrets
 import string
 import aiohttp
 from AylinRobot import AylinRobot as app
-
 from pyrogram import filters
 from Sehid import random_line
+from pyrogram. types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+button = InlineKeyboardMarkup([
 
-@app.on_message(filters.command("sehid") & ~filters.edited)
-async def commit(_, message):
-    await message.reply_text((await random_line('Sehid/sehid.txt')))
+[InlineKeyboardButton("Dəyiş", callback_data="deyis")]
+
+])
+
+@bot.on_message(filters.command("sehid") & "filters.edited)
+
+async def commit(_, message): await message.reply_text((await random_line('AykhanPro/txt/sehid.txt')), reply_markup-button)
+
+@bot.on_callback_query(filters.regex("deyis"))
+
+async def deyis(_, query: CallbackQuery):
+
+await query.edit_message_text((await random_line('Sehid/sehid.txt')), reply_markup-button)
