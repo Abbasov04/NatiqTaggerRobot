d936e741496177e50c92fd6d78581e0619716b9a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-01 22:21:53 +0400

    update Soxri🍑.py

--- AylinRobot/Plugin/Soxri🍑.py
+++ AylinRobot/Plugin/Soxri🍑.py
@@ -190,6 +190,7 @@ photolist = [
 
 @app.on_message(filters.command("soxri") & ~filters.edited)
 async def test_bot(bot: app, m: Message):
+    message.delete()  
     start = time()
     replymsg = await m.reply_text("**❤ Rondom Bir Şəkil Seçilir...**")
     end = round(time() - start, 2)
