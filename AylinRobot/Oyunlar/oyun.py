faabf37d27dfdcf48663f7b7740d05df4dbad514
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-01 16:29:00 +0400

    update oyun.py

--- AylinRobot/Oyunlar/oyun.py
+++ AylinRobot/Oyunlar/oyun.py
@@ -6,27 +6,79 @@ from AylinRobot import LOGGER
 from pyrogram import Client, filters
 
 
-@app.on_message(filters.command("zer"))
-async def roll_dice(bot, message):
-    await app.send_dice(message.chat.id, "🎲")
+
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
 
 
-@app.on_message(filters.command("ox"))                                      
-async def roll_arrow(bot, message):
-    await app.send_dice(message.chat.id, "🎯")
+DART_E_MOJI = "🎯"
+BOWLING = "🎳"
+TRY_YOUR_LUCK = "🎰"
+GOAL_E_MOJI = "⚽"
+BASKETBALL = "🏀"
 
-@app.on_message(filters.command("top"))
-async def roll_goal(bot, message):
-    await app.send_dice(message.chat.id, "⚽️")
 
-@app.on_message(filters.command("jackpot"))
-async def roll_luck(bot, message):
-    await app.send_dice(message.chat.id, "🎰")
+@app.on_message(filters.command(["throw"]))
+async def throw_dart(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DART_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
 
-@app.on_message(filters.command("basket"))
-async def roll_throw(bot, message):
-    await app.send_dice(message.chat.id, "🏀")
 
-@app.on_message(filters.command(["bowling"]))
-async def roll_bowling(bot, message):
-    await app.send_dice(message.chat.id, "🎳")
+@app.on_message(filters.command(["bg"]))
+async def bowling(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=BOWLING,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+
+@app.on_message(filters.command(["luck"]))
+async def luck_cownd(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=TRY_YOUR_LUCK,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+
+@app.on_message(filters.command(["ball"]))
+async def roll_dice(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=GOAL_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+
+
+@app.on_message(filters.command(["basket"))
+async def basketball(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=BASKETBALL,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+    
