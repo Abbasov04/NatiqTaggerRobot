c695816ff256c6d4e779a2c27655863d6c0c497a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 20:52:45 +0400

    update Sehid🇦🇿.py

--- AylinRobot/Plugin/Sehid🇦🇿.py
+++ AylinRobot/Plugin/Sehid🇦🇿.py
@@ -4,9 +4,21 @@
 
 import secrets, string, aiohttp
 from AylinRobot import AylinRobot as app
-from pyrogram import filters
 from Sehid import random_line
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from cryptography.fernet import Fernet
+
+
+button = InlineKeyboardMarkup([
+    [InlineKeyboardButton("🔄Dəyiş", callback_data="deyis")]
+])
 
 @app.on_message(filters.command("sehid") & ~filters.edited)
 async def commit(_, message):
-    await message.reply_text((await random_line('Sehid/sehid.txt')))
+    await message.reply_text((await random_line('Sehid/txt/sehid.txt')), reply_markup=button)
+
+
+@app.on_callback_query(filters.regex("deyis"))
+async def deyis(_, query: CallbackQuery):
+    await query.edit_message_text((await random_line('Sehid/txt/sehid.txt')), reply_markup=button)
