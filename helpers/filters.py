b0b78df6b68be9ff2caf1710857b0914547ece1b
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 14:45:40 +0400

    update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,11 +1,12 @@
-from typing import Union, List
-
 from pyrogram import filters
-
+from typing import List, Union
 from AylinRobot.config import Config
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
 
 
 def command(commands: Union[str, List[str]]):
