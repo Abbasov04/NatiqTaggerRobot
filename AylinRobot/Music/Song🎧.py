5baa16f42d22b0aef8f5e67b4061c35f6fda677e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 13:51:08 +0400

    update Song🎧.py

--- AylinRobot/Music/Song🎧.py
+++ AylinRobot/Music/Song🎧.py
@@ -28,9 +28,8 @@ buttons = {
 }
 
 
-@app.on_message(filters.command(['song']))
-def song(client, message):
-
+@app.on_message(filters.command(["song"]) & ~filters.edited)
+async def song(client, message):
     message.delete()
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
