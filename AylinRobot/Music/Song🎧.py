5bbf0e92d80911241eab4db41bf1a96f6834bf9b
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 14:34:23 +0400

    update Song🎧.py

--- AylinRobot/Music/Song🎧.py
+++ AylinRobot/Music/Song🎧.py
@@ -28,9 +28,9 @@ buttons = {
 }
 
 
-@app.on_message(filters.command(["song"]) & ~filters.edited)
+@app.on_message(filters.command(["song"])) & ~filters.edited)
 async def song(client, message):
-    m = await message.delete()  
+   message.delete()  
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
     chutiya = "["+user_name+"](tg://user?id="+str(user_id)+")"
