17a8acff277663e2a948521745e79474138635df
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 23:59:57 +0400

    update Sehid🇦🇿.py

--- AylinRobot/Plugin/Sehid🇦🇿.py
+++ AylinRobot/Plugin/Sehid🇦🇿.py
@@ -11,6 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @app.on_message(filters.command("sehid") & ~filters.edited)
 async def commit(_, message):
     await message.reply_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
+    
 button = InlineKeyboardMarkup([[InlineKeyboardButton("🔄Dəyiş", callback_data="deyis")]])
 
 @app.on_callback_query(filters.regex("deyis"))
