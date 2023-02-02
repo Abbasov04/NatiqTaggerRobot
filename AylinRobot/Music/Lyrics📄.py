da9f095db1082564ee1e5ec7cd3f970259e1f8d7
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 12:21:36 +0400

    update Lyrics📄.py

--- AylinRobot/Music/Lyrics📄.py
+++ AylinRobot/Music/Lyrics📄.py
@@ -18,8 +18,9 @@ from pyrogram.types import (
 )
 
 
-@app.on_message(filters.command("lyrics") & ~filters.edited)
+@app.on_message(filters.command(["lyrics"]) & ~filters.edited)
 async def lrsearch(_, message: Message):
+    message.delete()  
     if len(message.command) < 2:
         return await message.reply_text("**İstifadə:**\n\n/lyrics [ Musiqi Adı]")
     m = await message.reply_text("✍️ Mahnı sözləri axtarılır")
