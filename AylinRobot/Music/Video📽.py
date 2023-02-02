42015fac1e8b151b86ab96609353cfc65240b624
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 12:11:12 +0400

    update Video📽.py

--- AylinRobot/Music/Video📽.py
+++ AylinRobot/Music/Video📽.py
@@ -3,6 +3,7 @@
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
 import os, youtube_dl, requests, aiohttp, wget, time, yt_dlp
+from yt_dlp import YoutubeDL
 from youtube_search import YoutubeSearch
 from pyrogram import Client, filters
 from AylinRobot import AylinRobot as app
