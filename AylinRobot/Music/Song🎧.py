108c6f8d70e95d303f6d14d631911862f6b3540f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-02 10:37:27 +0400

    update Song🎧.py

--- AylinRobot/Music/Song🎧.py
+++ AylinRobot/Music/Song🎧.py
@@ -4,7 +4,7 @@
 
 from AylinRobot import AylinRobot as app
 from helpers.filters import command, other_filters
-import os, youtube_dl, requests, aiohttp, wget, time, yt_dlp, YoutubeSearch
+import os, youtube_dl, requests, aiohttp, wget, time, yt_dlp, youtube_search, pytube
 from pyrogram.handlers import MessageHandler
 from AylinRobot.config import Config
 from pyrogram import Client, filters
