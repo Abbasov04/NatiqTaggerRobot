15ef137e11bc5cf904e18c965287711c97062abc
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-03 16:13:05 +0400

    update cb_buttons.py

--- AylinRobot/Plugin/cb_buttons.py
+++ AylinRobot/Plugin/cb_buttons.py
@@ -81,4 +81,6 @@ async def cb_data(client, message):
             reply_markup=Button.TAGGER_BUTTONS,
             disable_web_page_preview=True
         )                
+    else:
+        await message.message.delete()
         
