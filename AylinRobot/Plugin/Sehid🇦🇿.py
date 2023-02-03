7bc101be16848b39b1640819dc504d60a38934a1
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-03 16:15:33 +0400

    update Sehid🇦🇿.py

--- AylinRobot/Plugin/Sehid🇦🇿.py
+++ AylinRobot/Plugin/Sehid🇦🇿.py
@@ -5,7 +5,6 @@
 import secrets, string, aiohttp
 from AylinRobot import AylinRobot as app
 from Sehid import random_line
-from AylinRobot.Plugin import Button
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
