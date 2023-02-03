743ef5cbb8de11d444ebae83d80cb7633ac50c70
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-04 00:57:09 +0400

    update Button.py

--- AylinRobot/Plugin/Button.py
+++ AylinRobot/Plugin/Button.py
@@ -65,10 +65,6 @@ InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),]])
 TAGGER_BUTTONS = InlineKeyboardMarkup(
 [[InlineKeyboardButton('🔐 Bağla', callback_data='close'),   
 InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),]])
-
-
 BH_BUTTONS = InlineKeyboardMarkup(
-[[InlineKeyboardButton('🎮 Oyunlar', callback_data='oyun'),
-InlineKeyboardButton('🇦🇿 Şəhidlər', callback_data='sehıd'),
-  InlineKeyboardButton('🔐 Bağla', callback_data='close'),   
+[[InlineKeyboardButton('🔐 Bağla', callback_data='close'),   
 InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),]])
