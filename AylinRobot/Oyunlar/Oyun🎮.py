678ea8033db8051ae5f4f4e7a4316e8e042295b5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-04 12:07:51 +0400

    update Oyun🎮.py

--- AylinRobot/Oyunlar/Oyun🎮.py
+++ AylinRobot/Oyunlar/Oyun🎮.py
@@ -13,6 +13,7 @@ BOWLING = "🎳"
 TRY_YOUR_LUCK = "🎰"
 GOAL_E_MOJI = "⚽"
 BASKETBALL = "🏀"
+DICE = "🎲"
 
 
 @app.on_message(filters.command(["ox"]))
@@ -78,4 +79,16 @@ async def basket(client, message):
         disable_notification=True,
         reply_to_message_id=rep_mesg_id
     )
-    
+
+@app.on_message(filters.command(["zer"]))
+async def basket(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DICE,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+        
