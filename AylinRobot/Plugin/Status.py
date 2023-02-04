52bb32422f9cbeafefecd8af5986c9b822fa6551
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-04 23:55:36 +0400

    update Status.py

--- AylinRobot/Plugin/Status.py
+++ AylinRobot/Plugin/Status.py
@@ -22,19 +22,12 @@ WDUR = False
 GRUP = []
 
 MENTION = "[{}](tg://user?id={})"
-MESSAGE = "Salam! {}, Əyləncə Dolu Qrupumuza Xoş Gəldin🥳!Qaydalara riaət etdikcə səndə favori userlərimizdən biri olacaqsan🤩!Əminəmki Nümunəvi Userlərdən biri olacaqsan!🥰"
-
 
 def btag():
 	BUTTON=[[InlineKeyboardButton(text="👨🏻‍💻Sahibim", url="https://t.me/sjrvan")]]
 	BUTTON=[[InlineKeyboardButton(text="Yeniliklər Kanalı📣", url="https://t.me/seninkanal")]]
 	return InlineKeyboardMarkup(BUTTON)
 
-
-def bstart():
-	BUTTON=[[InlineKeyboardButton(text="👨🏻‍💻Sahibim", url="https://t.me/sjrvan")]]
-	BUTTON+=[[InlineKeyboardButton(text="Məni Qrupa Əlavə Et✅", url=f"https://t.me/seninbot?startgroup=true")]]
-	return InlineKeyboardMarkup(BUTTON)
 
 @app.on_message(filters.command(["admin", "all"]) & filters.private)
 async def priw(client, message):
@@ -52,14 +45,14 @@ async def tag(client: app, message: Message):
 			await message.reply_text(f"{message.from_user.mention} Tag Prosesini Başlatdı! Hərkəsi Tag Edirəm Boss!⚡️",
 				reply_markup=btag()
 				)
-			time.sleep(1.5)
+			time.sleep(1)
 			SORGU = True
 			async for member in app.iter_chat_members(chat_id=chat.id, filter="all"):
 				if DUR:
 					DUR=False
 					SORGU = None
 					break
-				time.sleep(1.5)
+				time.sleep(1)
 				await app.send_message(chat_id=chat.id, text=f"{member.user.mention} {msg}")
 				time.sleep(1.5)
 		if message.from_user.id != mem.user.id:
@@ -107,14 +100,3 @@ async def stop(client: app, message: Message):
 			await message.reply_text(f"{message.from_user.mention} Tag prosesini dayandırdı❌ Tamam heçkəsi tag etmirəm😒")	
 		if message.from_user.id != mem.user.id:
 			pass
-	
-	
-@app.on_message(filters.group & filters.new_chat_members)
-def welcome(client, message):
-	global WDUR
-	global WSORGU
-	WSORGU=True
-	for i in message.new_chat_members:
-		new_members = MENTION.format(i.first_name, i.id)
-		text = MESSAGE.format(new_members)
-		message.reply(text, disable_web_page_preview=True)	
