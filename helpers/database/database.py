db2edc125c280815d424148c5ebdba628d1d83d6
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 21:15:41 +0400

    update database.py

--- helpers/database/database.py
+++ helpers/database/database.py
@@ -6,10 +6,77 @@ import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from AylinRobot.config import Config
 
-
 db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
 mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
 dcmdb = mongo_db_veritabani.handlers
 
 MONGODB_CLI = MongoClient(Config.MONGODB_URI)
 db = MONGODB_CLI.wbb
+
+
+class Database: 
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id): # Veritabanına yeni kullanıcı ekler
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason="",
+            ),
+        )
+
+    async def add_user(self, id): # Veritabına yeni kullanıcı eklemek için ön def
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id): # Bir kullanıcının veritabında olup olmadığını kontrol eder.
+        user = await self.col.find_one({"id": int(id)})
+        return bool(user)
+
+    async def total_users_count(self): # Veritabanındaki toplam kullanıcıları sayar.
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self): # Veritabındaki tüm kullanıcıların listesini verir.
+        return self.col.find({})
+
+    async def delete_user(self, user_id): # Veritabından bir kullanıcıyı siler.
+        await self.col.delete_many({"id": int(user_id)})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason): # Veritabanınızdaki bir kullanıcıyı yasaklılar listesine ekler.
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
+        )
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+
+    async def remove_ban(self, id): # Veritabanınızdaki yasaklılar listesinde bulunan bir kullanıcın yasağını kaldırır.
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
+    async def get_ban_status(self, id): # Bir kullanıcın veritabanınızda yasaklılar listesinde olup olmadığını kontrol eder.
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        user = await self.col.find_one({"id": int(id)})
+        return user.get("ban_status", default)
+
+    async def get_all_banned_users(self): # Veritabınızdaki yasaklı kullanıcılar listesini verir.
+        return self.col.find({"ban_status.is_banned": True})
