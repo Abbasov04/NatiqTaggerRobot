dc3df52a90f9ced9541e0edf0d10df4ec26cf7ea
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 20:49:58 +0400

    update database.py

--- helpers/database/database.py
+++ helpers/database/database.py
@@ -1,1 +1,13 @@
+# @AylinRobot
+# Sahib @HuseynH
+# Repo Açığdısa İcazəsis Götürmə Oğlum
 
+
+import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from AylinRobot.config import Config
+
+
+db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
+mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
+dcmdb = mongo_db_veritabani.handlers
