9e6e676cf3829cda772fd3ff088053434c590e0f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 21:31:54 +0400

    Add files via upload

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,5 +1,8 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
-
 __version__ = "0.7.0"
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from AylinRobot.config import Config
+
+
+MONGODB_CLI = MongoClient(Config.MONGODB_URI)
+db = MONGODB_CLI.wbb
