151261dd83aa19b44bf636df7f9b1c374bd1cc1e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 23:15:35 +0400

    update qarsılama.py

--- AylinRobot/Plugin/qarsılama.py
+++ AylinRobot/Plugin/qarsılama.py
@@ -8,9 +8,20 @@ from pyrogram import idle, filters
 from AylinRobot.config import Config
 from pyrogram import Client, filters
 
-xs = ["Salam,, və Qrupuna Xoş gəlmisiniz.",
-       "Dzzzzt Ddddddm",
-       "Xoş Gəldin Gözəlim",]
+@app.on_message(filters.command('list') & filters.group)
+async def start(client, msj):
+    BOTLAR = []
+    chat_id = msj.chat.id
+    async for members in app.iter_chat_members(chat_id):
+        botmu = members.user.is_bot
+        #print(botmu)
+        if botmu == True:
+            BOTLAR.append(members.user.mention)
+            botlarsiyahi = '\n'.join(BOTLAR)
+        else:
+            pass
 
+    botlarsiyahi = '\n'.join(BOTLAR)
+    await client.send_message(chat_id, botlarsiyahi)
 
 
