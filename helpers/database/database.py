315b641cf5028449a69980681f3242cc00df6a51
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 21:09:32 +0400

    update database.py

--- helpers/database/database.py
+++ helpers/database/database.py
@@ -5,10 +5,11 @@
 import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from AylinRobot.config import Config
-from AylinRobot.Plugin.Broadcast import Broadcast
+
 
 db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
 mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
 dcmdb = mongo_db_veritabani.handlers
+
 MONGODB_CLI = MongoClient(Config.MONGODB_URI)
 db = MONGODB_CLI.wbb
