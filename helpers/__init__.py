7cd77ce5bbf945a2bcc852415415c674cdb473c5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 20:42:19 +0400

    update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -3,10 +3,3 @@
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
 __version__ = "0.7.0"
-
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from AylinRobot.config import Config
-
-
-MONGODB_CLI = MongoClient(Config.MONGODB_URI)
-db = MONGODB_CLI.wbb
