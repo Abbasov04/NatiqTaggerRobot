05fceba53df0944149f4a7c51569a81933213711
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-05 11:45:10 +0400

    update Tag🛎.py

--- AylinRobot/Plugin/Tag🛎.py
+++ AylinRobot/Plugin/Tag🛎.py
@@ -18,9 +18,7 @@ SORGU = None
 MENTION = "[{}](tg://user?id={})"
 
 def btag():
-	BUTTON=
-[[InlineKeyboardButton(text="📝 Sahibim ", url=f"{Config.OWNER_NAME}")
-	InlineKeyboardButton(text="🎧 Playlist", url=f"{Config.PLAYLIST_NAME}")]]),
+	BUTTON=[[InlineKeyboardButton(text="📝 Sahibim ", url=f"{Config.OWNER_NAME}"), InlineKeyboardButton(text="🎧 Playlist", url=f"{Config.PLAYLIST_NAME}")]],
 	return InlineKeyboardMarkup(BUTTON)
 
 
