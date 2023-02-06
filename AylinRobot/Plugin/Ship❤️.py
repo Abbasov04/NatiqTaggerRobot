f7726a12737d6958e79ec9f8b456cb4b254f49ac
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-06 21:33:43 +0400

    update Ship❤️.py

--- AylinRobot/Plugin/Ship❤️.py
+++ AylinRobot/Plugin/Ship❤️.py
@@ -17,6 +17,8 @@ from AylinRobot.config import Config
 async def my_handler(client, msj):
     chat_id = msj.chat.id
 
+    w = await client.send_message(chat_id, f"Rondom Cütlük Seçilir")
+
     BU_QRUP_USERLERI = []
     async for member in client.iter_chat_members(chat_id):
         if member.user.is_bot == True:
@@ -38,12 +40,18 @@ async def my_handler(client, msj):
                 if rnduser == sevgi2:
                     await client.send_message(chat_id, f"{msj.from_user.mention} yeniden cehd edin")
                 elif rnduser != sevgi2:
-                    await client.send_message(chat_id,
-                                              f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
+                    time.sleep(2.5)
+                    await client.delete_messages(chat_id, w.id)
+                    await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
             elif rnduser != sevgi2:
-                await client.send_message(chat_id,
-                                          f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
+                time.sleep(2.5)
+                await client.delete_messages(chat_id, w.id)
+                await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
         elif rnduser != sevgi2:
+            time.sleep(2.5)
+            await client.delete_messages(chat_id, w.id)
             await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
     elif rnduser != sevgi2:
+        time.sleep(2.5)
+        await client.delete_messages(chat_id, w.id)
         await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
