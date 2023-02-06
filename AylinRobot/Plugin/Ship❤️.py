64cc0443d5b8bf0d0d901666f4717f9f33fdaad3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-06 22:00:55 +0400

    update Ship❤️.py

--- AylinRobot/Plugin/Ship❤️.py
+++ AylinRobot/Plugin/Ship❤️.py
@@ -18,7 +18,7 @@ from AylinRobot.config import Config
 async def my_handler(client, msj):
     chat_id = msj.chat.id
 
-    w = await client.send_message(chat_id, f"Rondom Cütlük Seçilir")
+    w = await client.send_message(chat_id, f"{Config.BOT_USERNAME} Sizin Üçün Rondom Cütlük Seçir")
 
     BU_QRUP_USERLERI = []
     async for member in client.iter_chat_members(chat_id):
