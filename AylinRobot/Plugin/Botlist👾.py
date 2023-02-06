301de8073f359f90adf49c2bed19135d8a96dc06
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-06 21:54:16 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -8,7 +8,6 @@ from AylinRobot import AylinRobot as app
 from AylinRobot import LOGGER
 from AylinRobot.config import Config
 from pyrogram import Client, idle, filters
-from pyrogram import enums
 
 @app.on_message(filters.command('list') & filters.group)
 async def start(client, msj):
@@ -24,19 +23,3 @@ async def start(client, msj):
 
     botlarsiyahi = '\n'.join(BOTLAR)
     await client.send_message(chat_id, botlarsiyahi)
-    
-    
-    
-# adminlist
-@app.on_message(filters.command('alist'))
-async def start(client, msj):
-    chat_id = msj.chat.id
-    reply = msj.reply_to_message
-    ADMINS = []
-    async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        ADMINS.append(m.user.mention)
-        adminsiyahi = '\n'.join(ADMINS)
-
-    #adminsiyahi = '\n'.join(ADMINS)
-    t = f"Qrupdaki adminler\n{adminsiyahi}"
-    await app.send_message(chat_id, t)    
