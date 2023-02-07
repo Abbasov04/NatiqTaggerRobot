0e2ccc0d75ec84a85fa577a53b8353d09a5f1c53
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-08 02:16:33 +0400

    update Ship❤️.py

--- AylinRobot/Plugin/Ship❤️.py
+++ AylinRobot/Plugin/Ship❤️.py
@@ -53,6 +53,6 @@ async def ship(client, msj):
             await client.delete_messages(chat_id, w.id)
             await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
     elif rnduser != sevgi2:
-        await sleep(2.5)
+        await sleep((2.5)
         await client.delete_messages(chat_id, w.id)
-        await client.send_message(chat_id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
+        await client.send_message(message.chat.id, f"Leyli ve Mecnun\n\n{rnduser} + {sevgi2} = {random.randint(0, 100)}%❤️")
