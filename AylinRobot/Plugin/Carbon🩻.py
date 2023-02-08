8ac2bac5a1275813edc2666c2f5bc9f2b373e456
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-08 23:32:49 +0400

    update Carbon🩻.py

--- AylinRobot/Plugin/Carbon🩻.py
+++ AylinRobot/Plugin/Carbon🩻.py
@@ -10,27 +10,24 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 #--------------------------------------------------------------
 
-
-aiohttpsession = ClientSession()
-
-
-async def get_http_status_code(url: str) -> int:
-    async with aiohttpsession.head(url) as resp:
-        return resp.status
-    
-
-async def make_carbon(code):
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiohttpsession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
-    image.name = "carbon.png"
-    return image
-
-@app.on_message(command(["carbon"]) & ~filters.edited)
-async def carbon_func(bot: app, msg: Message):
-    m = await msg.reply_text("`Hazırlanır`")
-    carbon = await make_carbon(msg.reply_to_message.text)
-    await m.edit("`Göndərilir`")
-    await bot.send_photo(msg.chat.id, photo=carbon)
-    await m.delete()
-    carbon.close()
+@app.on_message(filters.command("carbon"))
+async def carbon_func(client, msg):
+    reply = msg.reply_to_message
+    if reply:
+        m = await msg.reply_text("️🛎 Carbonu hazırlayıram...")
+        carbon = await make_carbon(msg.reply_to_message.text)
+        await m.edit("🎉 Artıq hazırdır! Göndərirəm.")
+        await client.send_document(msg.chat.id, carbon, caption=f"[OpenAI](https://t.me/openaimgbot) tərəfindən {msg.from_user.mention} üçün yaradıldı. 👻")
+        await m.delete()
+        carbon.close()
+    else:
+        try:
+            text = msg.text.split(" ", 1)[1]
+            m = await msg.reply_text("️🛎 Carbonu hazırlayıram...")
+            carbon = await make_carbon(text)
+            await m.edit("🎉 Artıq hazırdır! Göndərirəm.")
+            await client.send_document(msg.chat.id, carbon, caption="[OpenAI](https://t.me/openaimgbot) tərəfindən {msj.from_user.mention} üçün yaradıldı. 👻")
+            await m.delete()
+            carbon.close()
+        except IndexError:
+            await msg.reply_text("️🛎 Mətn daxil etmədin...")
