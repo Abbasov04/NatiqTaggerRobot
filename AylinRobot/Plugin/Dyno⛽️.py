13e8bf236ce5a28c74b772ea69846ed6953caf72
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-10 17:09:19 +0400

    update Dyno⛽️.py

--- AylinRobot/Plugin/Dyno⛽️.py
+++ AylinRobot/Plugin/Dyno⛽️.py
@@ -11,7 +11,7 @@ from AylinRobot.config import Config
 
 BOT_START_TIME = time.time()
 
-@app.on_message(filters.command(["dyno"]) & filters.user(Config.BOT_ID))  
+@app.on_message(filters.command(["dyno"]) & filters.user(Config.OWNER_ID))  
 async def bot_status(client,message):
     if Config.HEROKU_API_KEY:
         try:
