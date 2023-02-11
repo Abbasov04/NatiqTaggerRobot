ecfa243ddd0273870b2c68a3954ee4622c0e7d15
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 18:23:20 +0400

    update __init__.py

--- AylinRobot/Telethonedalet/__init__.py
+++ AylinRobot/Telethonedalet/__init__.py
@@ -2,11 +2,20 @@
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 
-from os.path import dirname, basename, isfile, join
-import glob
+from AylinRobot import *
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [
-    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
-]
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
 
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+print("Yüklənəcək modullar: ", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
