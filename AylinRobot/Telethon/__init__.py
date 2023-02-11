0dff2098c1d010886caa15c174887d1b5d69cf65
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 18:43:04 +0400

    update __init__.py

--- AylinRobot/Telethon/__init__.py
+++ AylinRobot/Telethon/__init__.py
@@ -1,1 +1,13 @@
+from telethon import events, Button
+from telethon import TelegramClient
+from telethon.tl.types import ChannelParticipantsAdmins
+import random
+from AylinRobot.config import Config
+from telethon import TelegramClient
 
+@client.on(events.NewMessage(pattern="^/test$"))
+async def start(event):
+  if event.is_private:
+     await event.reply(f"Test dəf kimi işləyir", buttons=(
+        [Button.url("👤 Sahib", url="https://t.me/edalet_22")],
+    ), 
