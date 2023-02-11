e809d6a54729578f29736e448229808f8bb7a4a4
Ədalət <99437747+EdaletRoBot@users.noreply.github.com>
2023-02-11 16:57:33 +0400

    Create test.py

--- AylinRobot/Telethonedalet/test.py
+++ AylinRobot/Telethonedalet/test.py
@@ -2,7 +2,6 @@
 # t.me/aykhan_s | t.me/edalet_22
 # GitHub: EdaletRoBot
 
-from pyrogram import Client
 from telethon import events
 from telethon import TelegramClient
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -10,36 +9,33 @@ import random
 from AylinRobot.config import Config
 
 
-AylinRobot = Client(
-    'AylinRobot',
-    bot_token = Config.BOT_TOKEN,
-    api_id = Config.API_ID,
-    api_hash = Config.API_HASH
-)
+edalet = TelegramClient('edalet', API_ID, API_HASH).start(bot_token=bot_token)
 
-SAHIB = 586648198
+API_ID = 8953338
+API_HASH = "fe21f223cb02d8f7c1cbda651f553a45"
+bot_token = "5910888289:AAHOmBFyKIwc4XtbiZnkOQWk2-EZtx6BrT8"
 
-@client.on(events.NewMessage(pattern="^/pin$"))
-async def pin(event):
-    if event.sender_id == SAHIB:
-        if not event.reply_to_msg_id:
-            return await event.reply("Bir mesajı cavablayın")
-        await event.reply("Pinləndi")
-        await event.client.pin_message(event.chat_id, event.reply_to_msg_id, notify=True)
+#@edalet_22 terefindən @RoBotlarimTg üçün yazilib silmədən istifadə edin
+@edalet.on(events.NewMessage(pattern="^/idid ?(.*)"))
+async def id(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_id = previous_message.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Sizin Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**İstifadəçi id:** `{user_id}`\n**Qrup id:** `{chat_id}`")
+
+
+#@edalet_22 terefindən @RoBotlarimTg üçün yazilib silmədən istifadə edin
     else:
-        await event.reply("Sən sahib deyilsən pinləməyə çalışma")
-
-@client.on(events.NewMessage(pattern="^/unpin$"))
-async def unpin(event):
-    if event.sender_id == SAHIB:
-        if not event.reply_to_msg_id:
-            return await event.reply("Bir pinlənən mesajı cavablayın")
-        await event.reply("Unpinləndi")
-        await event.client.unpin_message(event.chat_id)
-    else:
-        await event.reply("Sən sahib deyilsən unpinləməyə çalışma")
-
-
+        user_id = event.sender_id
+        chat_id = event.chat_id
+        if event.is_private:
+            return await event.reply(f"**Sizin Telegram id:** `{user_id}`")
+        else:
+            return await event.reply(f"**İstifadəçi id:** `{user_id}`\n**Qrup id:** `{chat_id}`")
 
 print(">> Bot işləyir narahat olmayın. @edalet_22 Məlumat almaq üçün <<")
 client.run_until_disconnected()
