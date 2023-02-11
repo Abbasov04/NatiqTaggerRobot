e9dbf2fb9f39576e3493dcf79ec77f1bc5873fe3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 16:40:36 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -17,7 +17,7 @@ from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from AylinRobot.Plugin import *
 from AylinRobot.Music import *
 from AylinRobot.Oyunlar import *
-from AylinRobot.Telethonedalet
+from AylinRobot.Telethonedalet import *
 from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AylinRobot import AylinRobot as app
