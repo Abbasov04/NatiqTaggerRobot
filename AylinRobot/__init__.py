73388531dcd9145f6e8a433852412ba32846dd2d
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 22:03:24 +0400

    update __init__.py

--- AylinRobot/__init__.py
+++ AylinRobot/__init__.py
@@ -6,6 +6,8 @@
 import logging
 from pyrogram import Client
 from AylinRobot.config import Config
+from telethon import TelegramClient
+from logging import *
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
