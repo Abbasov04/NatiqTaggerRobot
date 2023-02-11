78865a00d83b2328391f896b50515b10c92e2f0b
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 20:11:14 +0400

    update __init__.py

--- AylinRobot/__init__.py
+++ AylinRobot/__init__.py
@@ -3,26 +3,15 @@
 # Repo Açığdısa İcazəsis Götürmə Oğlum
 # Reponu Satan Kodları Götürən Kimliyindən Adlı Olmayaraq Peysərdi
 
-
 import logging
 from pyrogram import Client
 from AylinRobot.config import Config
-from AylinRobot import TelegramClient
-
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 LOGGER = logging.getLogger(__name__)
-
-AylinRobot = TelegramClient(
-   "Fallen", 
-    bot_token = Config.BOT_TOKEN,
-    api_id = Config.API_ID,
-    api_hash = Config.API_HASH
-)
-
 
 AylinRobot = Client(
     'AylinRobot',
