b558682bfc956195c8fc87fb00a8e17f3c09f2ca
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 22:07:49 +0400

    update __init__.py

--- AylinRobot/Telethon/__init__.py
+++ AylinRobot/Telethon/__init__.py
@@ -1,8 +1,26 @@
+# @TheXWarn - < https://t.me/TheXWarn >
+# Copyright (C) 2022
+#
+# Bu fayl, < https://github.com/TheXWarn/LuksTagBot >'un parçasıdır.
+# Zəhmət olmasa, GNU Affero Ümumi İctimai Lisenziyasını oxuyun;
+# < https://www.github.com/TheXWarn/LuksTagBot/blob/master/LICENSE/ >
+#
+#  Repodan nəsə əkib başqasına verən kimliyindən aslı olmayaraq peysərdi. # 
 
-from os.path import dirname, basename, isfile, join
-import glob
+from AylinRobot import *
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [
-    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
-]
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+print("Yüklənəcək modullar: ", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
