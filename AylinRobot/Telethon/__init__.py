f961a6b1b201130cfc46fa6feaa933dca9c232fe
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-11 19:29:38 +0400

    update __init__.py

--- AylinRobot/Telethon/__init__.py
+++ AylinRobot/Telethon/__init__.py
@@ -1,13 +1,8 @@
-from telethon import events, Button
-from telethon import TelegramClient
-from telethon.tl.types import ChannelParticipantsAdmins
-import random
-from AylinRobot.config import Config
-from telethon import TelegramClient
 
-@client.on(events.NewMessage(pattern="^/test$"))
-async def start(event):
-  if event.is_private:
-     await event.reply(f"Test dəf kimi işləyir", buttons=(
-        [Button.url("👤 Sahib", url="https://t.me/edalet_22")],
-    ), 
+from os.path import dirname, basename, isfile, join
+import glob
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
