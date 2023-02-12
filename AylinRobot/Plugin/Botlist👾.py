f46fe337cc502f35c8ce74c5e8d8ab47b7f0fb46
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-13 01:57:54 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -70,3 +70,51 @@ async def bots(client, message):
       await client.send_message(message.chat.id, text3)
   except FloodWait as e:
     await asyncio.sleep(e.value)
+    
+    
+    
+@app.on_message(filters.command(["admins","staff"]))
+async def admins(client, message):
+  try: 
+    adminList = []
+    ownerList = []
+    async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+      if admin.privileges.is_anonymous == False:
+        if admin.user.is_bot == True:
+          pass
+        elif admin.status == ChatMemberStatus.OWNER:
+          ownerList.append(admin.user)
+        else:  
+          adminList.append(admin.user)
+      else:
+        pass   
+    lenAdminList= len(ownerList) + len(adminList)  
+    text2 = f"**GROUP STAFF - {message.chat.title}**\n\n"
+    try:
+      owner = ownerList[0]
+      if owner.username == None:
+        text2 += f"👑 ᴏᴡɴᴇʀ\n└ {owner.mention}\n\n👮🏻 ᴀᴅᴍɪɴs\n"
+      else:
+        text2 += f"👑 ᴏᴡɴᴇʀ\n└ @{owner.username}\n\n👮🏻 ᴀᴅᴍɪɴs\n"
+    except:
+      text2 += f"👑 ᴏᴡɴᴇʀ\n└ <i>Hidden</i>\n\n👮🏻 ᴀᴅᴍɪɴs\n"
+    if len(adminList) == 0:
+      text2 += "└ <i>Admins are hidden</i>"  
+      await teletips.send_message(message.chat.id, text2)   
+    else:  
+      while len(adminList) > 1:
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"├ {admin.mention}\n"
+        else:
+          text2 += f"├ @{admin.username}\n"    
+      else:    
+        admin = adminList.pop(0)
+        if admin.username == None:
+          text2 += f"└ {admin.mention}\n\n"
+        else:
+          text2 += f"└ @{admin.username}\n\n"
+      text2 += f"**ᴊᴜᴍʟᴀʜ ᴛᴏᴛᴀʟ ᴀᴅᴍɪɴ**: {lenAdminList}\n❌ | ʙᴏᴛ sᴀᴍᴀ ᴀᴅᴋɪɴ ᴛᴇʀsᴇᴍʙᴜɴʏɪ ᴅɪᴛᴏʟᴀᴋ."  
+      await client.send_message(message.chat.id, text2)           
+  except FloodWait as e:
+    await asyncio.sleep(e.value)       
