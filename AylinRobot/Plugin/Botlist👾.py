2e4271d9dad1405860c5341a5e42061b5b546ea3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-13 01:20:00 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -10,6 +10,15 @@ from pyrogram.types import Message
 from AylinRobot import AylinRobot as app
 from AylinRobot.config import Config
 from pyrogram import Client, filters
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import os
+import asyncio
+from pyrogram import enums
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import FloodWait
+
+
 
 @app.on_message(filters.command('list') & filters.group)
 async def start(client, msj):
