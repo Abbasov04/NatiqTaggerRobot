6b2efb93a515358391f1a53b7dc911307d3af028
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-12 23:55:01 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -34,7 +34,7 @@ async def delAcc(client, msg):
     await msg.delete()
     await ms.reply("Silinən Hesablar Göndərilir")    
     await sleep(2.5)
-    await msg.delete()
+    await ms.delete()
     chat_id = msg.chat.id
     DELETED = []
     members = app.iter_chat_members(chat_id)
