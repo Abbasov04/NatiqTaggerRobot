2050519a03d8d408d2d6bfd9cd40d2f035eea1e5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-12 15:17:04 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -29,6 +29,7 @@ async def start(client, msj):
     
 @app.on_message(filters.command("del") & filters.group)
 async def delAcc(client, msj):
+   await message.delete()
     await msj.reply("Silinən Hesablar Axtarılır")
     await sleep(2.5)
     chat_id = msj.chat.id
@@ -39,6 +40,6 @@ async def delAcc(client, msj):
             DELETED.append(str(m.user.id)) # silinen hesablar
 
     shesablar = '\n'.join(DELETED) 
-    await app.send_message(chat_id, f"@{Config.BOT_USERNAME}\nSilinən Hesabları Tapdı Silinən Hesablar-  {len(DELETED)} 👻[{shesablar}] 🔍 Axtaran {msj.from_user.mention}") 
+    await app.send_message(chat_id, f"@{Config.BOT_USERNAME}\nSilinən Hesabları Tapdı Silinən Hesablar- 👻 {len(DELETED)} {shesablar} 🔍 Axtaran {msj.from_user.mention}") 
     
     
