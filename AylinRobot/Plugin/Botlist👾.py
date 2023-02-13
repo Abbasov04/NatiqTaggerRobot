422cc52453e68919b328d2deaa6b21758f73d1a1
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-13 16:56:19 +0400

    update Botlist👾.py

--- AylinRobot/Plugin/Botlist👾.py
+++ AylinRobot/Plugin/Botlist👾.py
@@ -141,15 +141,15 @@ async def admins(client, message):
       while len(adminList) > 1:
         admin = adminList.pop(0)
         if admin.username == None:
-          text2 += f"├ ❤️{admin.mention}\n"
+          text2 += f"├ ❤️ {admin.mention}\n"
         else:
-          text2 += f"├ ❤️@{admin.username}\n"    
+          text2 += f"├ ❤️ @{admin.username}\n"    
       else:    
         admin = adminList.pop(0)
         if admin.username == None:
-          text2 += f"└ ❤️{admin.mention}\n\n"
+          text2 += f"└ ❤️ {admin.mention}\n\n"
         else:
-          text2 += f"└ ❤️@{admin.username}\n\n"
+          text2 += f"└ ❤️ @{admin.username}\n\n"
       text2 += f"✅ | **İdarəçilərin ümumi sayı**: {lenAdminList}\n❌ | Botlar və gizli adminlər rədd edildi."  
       await app.send_message(message.chat.id, text2)           
   except FloodWait as e:
