7ca8545b9ac4d222c60881c97ea74fd95338b334
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-13 12:06:31 +0400

    update Sahib.py

--- AylinRobot/Plugin/Sahib.py
+++ AylinRobot/Plugin/Sahib.py
@@ -4,18 +4,4 @@ import random
 from AylinRobot import AylinRobot as app
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-import asyncio
-from pyrogram import Client, filters as Worker, emoji
 
-MENTION = "{}"  
-MESSAGE = "{} Xoş Gəldin {}!" 
-
-@app.on_message(Worker.new_chat_members)
-async def welcome(client, message):
-    new_members = [MENTION.format(message.from_user.mention) for i in message.new_chat_members]
-
-    text = MESSAGE.format(emoji.LOVE, ", ".join(new_members))
-
-    dell=await message.reply_text(text, disable_web_page_preview=True)
-    await asyncio.sleep(1000)
-    await dell.delete()
