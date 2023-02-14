d87e033a7d616a602bd9a5430d969768084d6e86
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 01:28:10 +0400

    update Sahib.py

--- AylinRobot/Plugin/Sahib.py
+++ AylinRobot/Plugin/Sahib.py
@@ -1,21 +1,103 @@
 import os
 from AylinRobot import AylinRobot as app
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from AylinRobot.config import Config
+import random
+from AylinRobot.sorular import D_LİST, C_LİST
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+# ============================ #
+
+# Dc Komutu İcin Olan Buttonlar
+def d_or_c(user_id):
+	BUTTON = [[InlineKeyboardButton(text="? Doğruluk", callback_data = " ".join(["d_data",str(user_id)]))]]
+	BUTTON += [[InlineKeyboardButton(text="?? Cesaret", callback_data = " ".join(["c_data",str(user_id)]))]]
+	return InlineKeyboardMarkup(BUTTON)
+
+# Dc Komutunu Oluşturalım
+@app.on_message(filters.command("dc"))
+async def _(client, message):
+	user = message.from_user
+
+	await message.reply_text(text="{} İstədiyiniz Sual Tipini seçin!".format(user.mention),
+		reply_markup=d_or_c(user.id)
+		)
+
+# Buttonlarımızı Yetkilendirelim
+@app.on_callback_query()
+async def _(client, callback_query):
+	d_soru=random.choice(D_LİST) # Random Bir Doğruluk Sorusu Seçelim
+	c_soru=random.choice(C_LİST) # Random Bir Cesaret Sorusu Seçelim
+	user = callback_query.from_user # Kullanıcın Kimliğini Alalım
+
+	c_q_d, user_id = callback_query.data.split() # Buttonlarımızın Komutlarını Alalım
+
+	# Sorunun Sorulmasını İsteyen Kişinin Komutu Kullanan Kullanıcı Olup Olmadığını Kontrol Edelim
+	if str(user.id) == str(user_id):
+		# Kullanıcının Doğruluk Sorusu İstemiş İse Bu Kısım Calışır
+		if c_q_d == "d_data":
+			await callback_query.answer(text="Doğruluq Sualını İstədiniz", show_alert=False) # İlk Ekranda Uyarı Olarak Gösterelim
+			await client.delete_messages(
+				chat_id=callback_query.message.chat.id,
+				message_ids=callback_query.message.message_id) # Eski Mesajı Silelim
+
+			await callback_query.message.reply_text("**{user} Doğruluq Sualı İstədi:** __{d_soru}__".format(user=user.mention, d_soru=d_soru)) # Sonra Kullanıcıyı Etiketleyerek Sorusunu Gönderelim
+			return
+
+		if c_q_d == "c_data":
+			await callback_query.answer(text="Cəsarət Sualını İstədiniz", show_alert=False)
+			await client.delete_messages(
+				chat_id=callback_query.message.chat.id,
+				message_ids=callback_query.message.message_id)
+			await callback_query.message.reply_text("**{user} Cəsarət Sualı İstədi** __{c_soru}__".format(user=user.mention, c_soru=c_soru))
+			return
 
 
-@app.on_message(filters.command('m') & filters.private)
-async def start(client, message):
-    await message.reply(
-        f"""        
-<b>First name</b>: {message.from_user.first_name}
-<b>Last name</b>: {message.from_user.last_name}
-<b>Username</b>: {message.from_user.username}
-<b>Telegram id</b>: <code>{message.from_user.id}</code>
-<b>Phone number</b>: {message.from_user.phone_number}
-<b>Language</b>: {message.from_user.language_code}
-<b>Status</b>: {str(message.from_user.status)[11:]}
-<b>Data center id</b>: {message.from_user.dc_id}
-<b>Type</b>: {str(message.reply_to_message.forward_from_chat.type)[9:]}
-"""
-)
+	# Buttonumuza Tıklayan Kisi Komut Calıştıran Kişi Değil İse Uyarı Gösterelim
+	else:
+		await callback_query.answer(text="Siz əmrdən istifadə edən şəxs deyilsiniz!", show_alert=False)
+		return
+
+############################
+    # Sudo islemleri #
+@app.on_message(filters.command("cekle"))
+async def _(client, message):
+  global MOD
+  user = message.from_user
+  
+  if user.id not in OWNER_ID:
+    await message.reply_text("**[?]** **Sən Admin Deyilsən!**")
+    return
+  MOD="cekle"
+  await message.reply_text("**[?]** **Əlavə etmək istədiyiniz Cəsarət Sualını daxil edin!**")
+  
+@app.on_message(filters.command("dekle"))
+async def _(client, message):
+  global MOD
+  user = message.from_user
+  
+  if user.id not in Config.OWNER_ID:
+    await message.reply_text("**[?]** **Sən Admin Deyilsən!**")
+    return
+  MOD="cekle"
+  await message.reply_text("**[?]** **Əlavə etmək istədiyiniz Doğruluq Sualını daxil edin!**")
+
+@app.on_message(filters.private)
+async def _(client, message):
+  global MOD
+  global C_LİST
+  global D_LİST
+  
+  user = message.from_user
+  
+  if user.id in Config.OWNER_ID:
+    if MOD=="cekle":
+      C_LİST.append(str(message.text))
+      MOD=None
+      await message.reply_text("**[?]** __Mətn Cəsarət Sualı kimi əlavə edildi!__")
+      return
+    if MOD=="dekle":
+      C_LİST.append(str(message.text))
+      MOD=None
+      await message.reply_text("**[?]** __Mətn Doğruluq Sualı kimi əlavə edildi!__")
+      return
