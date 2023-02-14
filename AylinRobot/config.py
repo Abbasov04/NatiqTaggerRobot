d62ce3f758a06cc28da7227b1d44328afff8c83c
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-14 12:17:37 +0400

    update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -6,6 +6,16 @@
 
 
 import os
+from os import getenv
+from dotenv import load_dotenv
+from KennedyMusic.helpers.uptools import fetch_heroku_git_url
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+que = {}
+admins = {}
 
 class Config:
 
