3a00a3f5e72a8c56e49d18f2fb8c67ac3c0f5ebe
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 00:28:24 +0400

    update Sahib.py

--- AylinRobot/Plugin/Sahib.py
+++ AylinRobot/Plugin/Sahib.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@app.on_message(filters.command('whois') & filters.private)
+@app.on_message(filters.command('m') & filters.private)
 async def start(client, message):
     await message.reply(
         f"""        
@@ -16,4 +16,30 @@ async def start(client, message):
 <b>Language</b>: {message.from_user.language_code}
 <b>Status</b>: {str(message.from_user.status)[11:]}
 <b>Data center id</b>: {message.from_user.dc_id}"""
+)
+
+@app.on_message(filters.command('u') & filters.private)
+async def start(client, message):
+    await message.reply(
+        f"""
+<b>First name</b>: {message.reply_to_message.forward_from.first_name}
+<b>Last name</b>: {message.reply_to_message.forward_from.last_name}
+<b>Username</b>: {message.reply_to_message.forward_from.username}
+<b>Telegram id</b>: <code>{message.reply_to_message.forward_from.id}</code>
+<b>Phone number</b>: {message.reply_to_message.forward_from.phone_number}
+<b>Language</b>: {message.reply_to_message.forward_from.language_code}
+<b>Status</b>: {str(message.reply_to_message.forward_from.status)[11:]}
+<b>Data center id</b>: {message.reply_to_message.forward_from.dc_id}"""
+)
+
+@app.on_message(filters.command('c'))
+async def start(client, message):
+    await message.reply(
+        f"""
+<b>Name</b>: {message.reply_to_message.forward_from_chat.title}
+<b>Username</b>: {message.reply_to_message.forward_from_chat.username}
+<b>Telegram id</b>: {message.reply_to_message.forward_from_chat.id}
+<b>Type</b>: {str(message.reply_to_message.forward_from_chat.type)[9:]}
+<b>Data center id</b>: {message.reply_to_message.forward_from_chat.dc_id}
+"""
 )
