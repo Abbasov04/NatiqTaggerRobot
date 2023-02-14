8fde05bc0103444edb4af53dd1638f87e01a95cd
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 00:23:47 +0400

    update Sahib.py

---
+++ AylinRobot/Plugin/Sahib.py
@@ -0,0 +1,19 @@
+import os
+from AylinRobot import AylinRobot as app
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(filters.command('whois') & filters.private)
+async def start(client, message):
+    await message.reply(
+        f"""        
+<b>First name</b>: {message.from_user.first_name}
+<b>Last name</b>: {message.from_user.last_name}
+<b>Username</b>: {message.from_user.username}
+<b>Telegram id</b>: <code>{message.from_user.id}</code>
+<b>Phone number</b>: {message.from_user.phone_number}
+<b>Language</b>: {message.from_user.language_code}
+<b>Status</b>: {str(message.from_user.status)[11:]}
+<b>Data center id</b>: {message.from_user.dc_id}"""
+)
