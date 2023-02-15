5f69ee1b40d4990ebb09dd491978aa31cdda4bca
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 19:10:24 +0400

    update Broadcast📣.py

--- AylinRobot/Plugin/Broadcast📣.py
+++ AylinRobot/Plugin/Broadcast📣.py
@@ -117,21 +117,3 @@ async def broadcast_message(_, message):
         except Exception:
             pass
     await m.edit(f"✈️ **Broadcasted message in {sent} chats.**")
-
-
-
-@app.on_message(filters.command("stats") & filters.user(Config.OWNER_ID))
-async def show_status_count(_, client: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await client.reply_text(
-        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@leosongdownloaderbot 🇱🇰",
-        parse_mode="Markdown",
-        quote=True
-    )
