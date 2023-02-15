509c7c9eda2f0d86d37a65b4d7948c2afe3eb3a2
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 21:39:33 +0400

    update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -7,21 +7,21 @@ from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
-async def ForceSub(app: Client, event: Message):
+async def ForceSub(bot: Client, event: Message):
 
     try:
-        invite_link = await app.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL)))
+        invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL)))
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(app, event)
+        fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
         print(f"{Config.UPDATES_CHANNEL} kanalına məcburi abunə olmaq mümkün deyil\n\nXəta: {err}\n\nDəstək Qrupu ilə əlaqə saxlayın: @{Config.CHANNEL}")
         return 200
     try:
-        user = await app.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL)), user_id=event.from_user.id)
+        user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL)), user_id=event.from_user.id)
         if user.status == "kicked":
-            await app.send_message(
+            await bot.send_message(
                 chat_id=event.from_user.id,
                 text=f"Üzr istəyirik, əzizim, məndən istifadə etmək qadağandır ☹️\nDəstək Qrupumuzda @{Config.CHANNEL} bildirməkdən çəkinməyin..",
                 parse_mode="markdown",
@@ -32,7 +32,7 @@ async def ForceSub(app: Client, event: Message):
         else:
             return 200
     except UserNotParticipant:
-        await app.send_message(
+        await bot.send_message(
             chat_id=event.from_user.id,
             text="<b>Salam {} 👋\n\nYeniləmələr Kanalımıza abunə olana qədər məndən istifadə edə bilməzsiniz ☹️\n\nOdur ki, aşağıdakı düymə ilə Yeniləmələr Kanalımıza qoşulun və 'Yenilə 🔄' düyməsini sıxın 😊</b>".format(event.from_user.mention),
             reply_markup=InlineKeyboardMarkup(
@@ -52,7 +52,7 @@ async def ForceSub(app: Client, event: Message):
         return 400
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(app, event)
+        fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
         print(f"Nə isə səhv getdi!  Məcburi Abunə olmaq mümkün deyil.\nXəta: {err}\n\nDəstək Qrupu ilə əlaqə saxlayın: @{Config.CHANNEL}")
