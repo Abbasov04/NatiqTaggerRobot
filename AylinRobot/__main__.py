2e094dce44bc2dff4977ec2298027776d51c2b08
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 21:55:19 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -27,7 +27,7 @@ from AylinRobot import LOGGER
 
 AylinIMG = f"{Config.START_IMG}"
 
-@app.on_message(filters.command(["start"]))
+@app.on_message(filters.private & filters.incoming & filters.command(['start']))
 async def start(client, message):
     await AddUserToDatabase(client, message)
     FSub = await ForceSub(client, message)
