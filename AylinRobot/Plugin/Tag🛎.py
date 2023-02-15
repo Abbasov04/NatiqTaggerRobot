82807fb5bb03f29143118b5976cd74e192b22e9e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-16 00:18:24 +0400

    update Tag🛎.py

--- AylinRobot/Plugin/Tag🛎.py
+++ AylinRobot/Plugin/Tag🛎.py
@@ -15,7 +15,7 @@ async def logger(client, msj):
         await client.delete_messages(chat_id, msj.id)
         await client.forward_messages(
             chat_id=chat_id,
-            from_chat_id=msj.chat.id, {Config.LOG_CHANNEL},
+            from_chat_id=msj.chat.id, (Config.LOG_CHANNEL),
             message_ids=replyid))
         await client.send_message(chat_id, f"{msj.from_user.mention} mesaj uğurla log qrupuna göndərildi.")
     else:
