56e05a356420a99ba5fb7e20bd42e211899d574a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 11:44:01 +0400

    update Urek.py

--- AylinRobot/Plugin/Urek.py
+++ AylinRobot/Plugin/Urek.py
@@ -5,7 +5,6 @@ from random import choice
 from AylinRobot import AylinRobot as app
 from pyrogram import filters
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
 
 
 
@@ -14,18 +13,18 @@ from pyrogram.errors import FloodWait
 def startFunc(client, msj):
     chat_id = msj.chat.id
     urekler = ['💙','💚','💛','💜','🤍','🤎','♥️']
-   await  m = msj
-   await a = client.send_message(chat_id, f"💙💚💛💜🤍🤎♥️")
-    await sleep(1.6)
+    m = msj
+    a = client.send_message(chat_id, f"💙💚💛💜🤍🤎♥️")
+    time.sleep(1.6)
     w = a.edit('💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️')
-    await sleep(1.6)
+    time.sleep(1.6)
     z = w.edit('💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️')
-    await sleep(1.6)
+    time.sleep(1.6)
     t = z.edit('💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️')
-    await sleep(1.6)
+    time.sleep(1.6)
     g = t.edit('💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️')
     #time.sleep(1.6)
     #son = g.edit('💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️\n💙💚💛💜🤍🤎♥️')
-    await sleep(0.7)
+    time.sleep(0.7)
     client.delete_messages(chat_id, g.id)
     client.send_message(chat_id, f"{random.choice(urekler)}")
