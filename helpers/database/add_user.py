106c1e98e18c61f9d4b5870fc35d7844c53e2b80
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 18:35:18 +0400

    update add_user.py

--- helpers/database/add_user.py
+++ helpers/database/add_user.py
@@ -7,8 +7,8 @@ from pyrogram.types import Message
 async def AddUserToDatabase(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if config.LOG_CHANNEL is not None:
+        if Config.LOG_CHANNEL is not None:
             await bot.send_message(
-                int(config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
+                int(Config.LOG_CHANNEL),
+                f"♒️ #YENİ_İSTİFADƏÇİ: \n\n💁‍♀️ YENİ İSTİFADƏÇİ [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) Bot 🙎‍♀️ @{(await bot.get_me()).username} !!"
             )
