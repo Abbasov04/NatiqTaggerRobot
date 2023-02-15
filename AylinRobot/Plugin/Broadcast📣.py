acec3638a7cc29c08d3d4b2369eae29eb0120c1e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 17:34:21 +0400

    update Broadcast📣.py

--- AylinRobot/Plugin/Broadcast📣.py
+++ AylinRobot/Plugin/Broadcast📣.py
@@ -1,12 +1,9 @@
-import shutil, psutil, traceback, os, datetime, random, string, time, traceback, aiofiles, asyncio
+import asyncio
+from pyrogram import filters
 from pyrogram.errors import FloodWait
 from AylinRobot.config import Config
 from AylinRobot import AylinRobot as app
-import motor.motor_asyncio
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from pyrogram.types import Message
-from pyrogram import Client, filters, __version__ as USER
-from AylinRobot.translation import *
+from pyrogram import Client as USER
 from helpers.chats import add_served_chat, blacklisted_chats, get_served_chats
 
 chat_watcher_group = 10
@@ -29,7 +26,7 @@ async def chat_watcher_func(_, message):
     await add_served_chat(chat_id)
 
 
-@app.on_message(filters.command(["broadcast_pin"]) & filters.user(Config.OWNER_ID))
+@app.on_message(filters.command("broadcast_pin") & filters.user(Config.OWNER_ID))
 async def broadcast_message(_, message):
     if not message.reply_to_message:
         pass
@@ -88,7 +85,7 @@ async def broadcast_message(_, message):
 # Broadcast without pinned
 
 
-@app.on_message(filters.command(["gcast"]) & filters.user(Config.OWNER_ID))
+@app.on_message(filters.command("gcast") & filters.user(Config.OWNER_ID))
 async def broadcast_message(_, message):
     if len(message.command) < 2:
         return await message.reply_text("**Usage**:\n/gcast [message]")
@@ -110,133 +107,3 @@ async def broadcast_message(_, message):
         except Exception:
             pass
     await m.edit(f"✈️ **Broadcasted message in {sent} chats.**")
-
-
-@app.on_message(filters.command(["stats"]) & filters.user(Config.OWNER_ID))
-async def botstats(app: Client, message: Message):
-    g4rip = await app.send_message(message.chat.id, LAN.STATS_STARTED.format(message.from_user.mention))
-    all_users = await db.get_all_users()
-    groups = 0
-    pms = 0
-    async for user in all_users:
-        if str(user["id"]).startswith("-"):
-            groups += 1
-        else:
-            pms += 1
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage("/").percent
-    total_users = await db.total_users_count()
-    await g4rip.edit(text=LAN.STATS.format(Config.BOT_USERNAME, total_users, groups, pms, total, used, disk_usage, free, cpu_usage, ram_usage, __version__))
-
-
-class Database: 
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
-
-    def new_user(self, id): # Veritabanına yeni kullanıcı ekler
-        return dict(
-            id=id,
-            join_date=datetime.date.today().isoformat(),
-            ban_status=dict(
-                is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
-                ban_reason="",
-            ),
-        )
-
-    async def add_user(self, id): # Veritabına yeni kullanıcı eklemek için ön def
-        user = self.new_user(id)
-        await self.col.insert_one(user)
-
-    async def is_user_exist(self, id): # Bir kullanıcının veritabında olup olmadığını kontrol eder.
-        user = await self.col.find_one({"id": int(id)})
-        return bool(user)
-
-    async def total_users_count(self): # Veritabanındaki toplam kullanıcıları sayar.
-        count = await self.col.count_documents({})
-        return count
-
-    async def get_all_users(self): # Veritabındaki tüm kullanıcıların listesini verir.
-        return self.col.find({})
-
-    async def delete_user(self, user_id): # Veritabından bir kullanıcıyı siler.
-        await self.col.delete_many({"id": int(user_id)})
-
-    async def ban_user(self, user_id, ban_duration, ban_reason): # Veritabanınızdaki bir kullanıcıyı yasaklılar listesine ekler.
-        ban_status = dict(
-            is_banned=True,
-            ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason,
-        )
-        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
-
-    async def remove_ban(self, id): # Veritabanınızdaki yasaklılar listesinde bulunan bir kullanıcın yasağını kaldırır.
-        ban_status = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
-        )
-        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
-
-    async def get_ban_status(self, id): # Bir kullanıcın veritabanınızda yasaklılar listesinde olup olmadığını kontrol eder.
-        default = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
-        )
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("ban_status", default)
-
-    async def get_all_banned_users(self): # Veritabınızdaki yasaklı kullanıcılar listesini verir.
-        return self.col.find({"ban_status.is_banned": True})
-
-
-db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
-mongo_db_veritabani = MongoClient(Config.MONGODB_URI)
-dcmdb = mongo_db_veritabani.handlers
-
-
-
-################## KULLANICI KONTROLLERİ #############
-async def handle_user_status(bot: Client, cmd: Message): # Kullanıcı kontrolü
-    chat_id = cmd.chat.id
-    if not await db.is_user_exist(chat_id):
-        if cmd.chat.type == "private":
-            await db.add_user(chat_id)
-            await app.send_message(Config.LOG_CHANNEL,LAN.BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id))
-        else:
-            await db.add_user(chat_id)
-            chat = bot.get_chat(chat_id)
-            if str(chat_id).startswith(f"{Config.LOG_CHANNEL}"):
-                new_chat_id = str(chat_id)[4:]
-            else:
-                new_chat_id = str(chat_id)[1:]
-            await app.send_message(Config.LOG_CHANNEL,LAN.GRUP_BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id, chat.title, cmd.chat.id, cmd.chat.id, cmd.message_id))
-
-@app.on_message()
-async def G4RIP(app: Client, cmd: Message):
-    await handle_user_status(app, cmd)
-
-
-
-def humanbytes(size):
-    if not size:
-        return ""
-    power = 2**10
-    raised_to_pow = 0
-    dict_power_n = {0: "", 1: "K", 2: "M", 3: "G", 4: "T"}
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"            
