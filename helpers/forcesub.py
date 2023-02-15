5074e715a7cd62bc043490822e7bace150721d71
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-15 22:03:28 +0400

    update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -10,20 +10,20 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 async def ForceSub(bot: Client, event: Message):
 
     try:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL)))
+        invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.PLAYLIST_NAME)))
     except FloodWait as e:
         await asyncio.sleep(e.x)
         fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
-        print(f"{Config.UPDATES_CHANNEL} kanalına məcburi abunə olmaq mümkün deyil\n\nXəta: {err}\n\nDəstək Qrupu ilə əlaqə saxlayın: @{Config.CHANNEL}")
+        print(f"{Config.PLAYLIST_NAME} kanalına məcburi abunə olmaq mümkün deyil\n\nXəta: {err}\n\Sahibimlə: 👨‍💻 @{Config.OWNER_NAME} Əlaqə Saxla")
         return 200
     try:
         user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL)), user_id=event.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=event.from_user.id,
-                text=f"Üzr istəyirik, əzizim, məndən istifadə etmək qadağandır ☹️\nDəstək Qrupumuzda @{Config.CHANNEL} bildirməkdən çəkinməyin..",
+                text=f" Məndən istifadə etmək qadağandır ☹️\nSahibimlə 👨‍💻 @{Config.OWNER_NAME} Əlaqə Saxla..",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
                 reply_to_message_id=event.message_id
@@ -34,15 +34,15 @@ async def ForceSub(bot: Client, event: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=event.from_user.id,
-            text="<b>Salam {} 👋\n\nYeniləmələr Kanalımıza abunə olana qədər məndən istifadə edə bilməzsiniz ☹️\n\nOdur ki, aşağıdakı düymə ilə Yeniləmələr Kanalımıza qoşulun və 'Yenilə 🔄' düyməsini sıxın 😊</b>".format(event.from_user.mention),
+            text="<b>Salam {} 👋\n\nKanalımıza abunə olana qədər məndən istifadə edə bilməzsiniz ☹️\n\nOdur ki, aşağıdakı düymə ilə Kanalımıza qoşulun və '🙋‍♀️ Yenilə' düyməsini sıxın 😊</b>".format(event.from_user.mention),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("👨‍💻 Yeniliklər Kanalı", url=invite_link.invite_link)
+                        InlineKeyboardButton("📣 Kanalımız", url=invite_link.invite_link)
                     ],
 
                     [   
-                        InlineKeyboardButton("Yenilə 🔄", callback_data="refreshme")
+                        InlineKeyboardButton("🙋‍♀️ Yenilə", callback_data="refreshme")
                     ],
                 ]
             ),
@@ -55,5 +55,5 @@ async def ForceSub(bot: Client, event: Message):
         fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
-        print(f"Nə isə səhv getdi!  Məcburi Abunə olmaq mümkün deyil.\nXəta: {err}\n\nDəstək Qrupu ilə əlaqə saxlayın: @{Config.CHANNEL}")
+        print(f"Nə isə səhv getdi!  Məcburi Abunə olmaq mümkün deyil.\nXəta: {err}\n\nSahibim İlə @{Config.OWNER_NAME} Əlaqə Saxla")
         return 200
