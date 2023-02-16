e32d4b7df11f86ce37db0fb35400851727cf4038
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-16 17:48:25 +0400

    update Test.py

--- AylinRobot/Plugin/Test.py
+++ AylinRobot/Plugin/Test.py
@@ -14,6 +14,24 @@ from pyrogram import Client, filters, __version__
 from pyrogram.errors import FloodWait
 
 async def handle_user_status(app: Client, cmd: Message):
+    chat_id = cmd.chat.id
+    if not await db.is_user_exist(chat_id):
+        if cmd.chat.type == "private":
+            await db.add_user(chat_id)
+            await app.send_message(Config.LOG_CHANNEL,LAN.BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id))
+        else:
+            await db.add_user(chat_id)
+            chat = bot.get_chat(chat_id)
+            if str(chat_id).startswith(f"{Config.LOG_CHANNEL}"):
+                new_chat_id = str(chat_id)[4:]
+            else:
+                new_chat_id = str(chat_id)[1:]
+            await app.send_message(Config.LOG_CHANNEL,LAN.GRUP_BILDIRIM.format(cmd.from_user.first_name, cmd.from_user.id, cmd.from_user.first_name, cmd.from_user.id, chat.title, cmd.chat.id, cmd.chat.id, cmd.message_id))
+
+
+
+
+
 
 
 @app.on_message(filters.command(["stats"]) & filters.user(Config.OWNER_ID))
@@ -57,3 +75,5 @@ def humanbytes(size):
         size /= power
         raised_to_pow += 1
     return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
