aa564747a75022d7356c52479cfd97c02975a28b
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-16 20:21:32 +0400

    update Test.py

--- AylinRobot/Plugin/Test.py
+++ AylinRobot/Plugin/Test.py
@@ -37,9 +37,37 @@ class Database:
         await self.col.insert_one(user)
 
     async def is_user_exist(self, id): # Bir kullanıcının veritabında olup olmadığını kontrol eder.
+        user = await self.col.find_one({"id": int(id)})
+        return bool(user)
 
-    async def total_users_count(self):
-      
+    async def total_users_count(self): # Veritabanındaki toplam kullanıcıları sayar.
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self): # Veritabındaki tüm kullanıcıların listesini verir.
+        return self.col.find({})
+
+    async def delete_user(self, user_id): # Veritabından bir kullanıcıyı siler.
+        await self.col.delete_many({"id": int(user_id)})
+
+    async def ban_user(self, user_id, ban_duration, ban_reason): # Veritabanınızdaki bir kullanıcıyı yasaklılar listesine ekler.
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason,
+        )
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
+
+    async def remove_ban(self, id): # Veritabanınızdaki yasaklılar listesinde bulunan bir kullanıcın yasağını kaldırır.
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason="",
+        )
+        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
+
 
 
 db = Database(Config.MONGODB_URI, Config.BOT_USERNAME)
