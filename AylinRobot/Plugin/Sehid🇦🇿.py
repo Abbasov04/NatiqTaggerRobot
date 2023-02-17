60fc8bf2ff383b9f1b257ea8c18c015ecf093beb
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-17 09:02:18 +0400

    update Sehid🇦🇿.py

--- AylinRobot/Plugin/Sehid🇦🇿.py
+++ AylinRobot/Plugin/Sehid🇦🇿.py
@@ -9,10 +9,10 @@ from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@app.on_message(filters.command("game"))
+@app.on_message(filters.command("sehid"))
 async def _(client, message):
 	user = message.from_user
-	await message.reply_text(text="{} Doğruluq yoxsa cəsarət!".format(user.mention, await random_line('Sehid/sehid.txt')), reply_markup=button)
+	await message.reply_text(text="{} Əmri İcra Etdi!".format(user.mention, await random_line('Sehid/sehid.txt')), reply_markup=button)
     
 button = reply_markup=InlineKeyboardMarkup(
 [[InlineKeyboardButton("🇦🇿 Şəhid", callback_data="deyis"),
