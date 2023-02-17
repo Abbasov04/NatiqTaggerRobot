05b653e3e6be6f3103e6ed82ec4b11c6c96446cc
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-17 21:38:54 +0400

    update Broadcast📣.py

--- AylinRobot/Plugin/Broadcast📣.py
+++ AylinRobot/Plugin/Broadcast📣.py
@@ -40,7 +40,7 @@ async def broadcast_message(_, message):
     if not message.reply_to_message:
         pass
     else:
-        x = message.reply_to_message.message_id
+        x = message.reply_to_message.id
         y = message.chat.id
         sent = 0
         pin = 0
