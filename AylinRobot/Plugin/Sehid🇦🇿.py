1f79f3c389487fb148e13340e5fc4e5afa1438cd
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-17 08:46:21 +0400

    update Sehid🇦🇿.py

--- AylinRobot/Plugin/Sehid🇦🇿.py
+++ AylinRobot/Plugin/Sehid🇦🇿.py
@@ -12,15 +12,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @app.on_message(filters.command(["sehid"]))
 async def commit(_, message):
    await message.delete()  
-    await message.reply_text((await random_line('Sehid/sehid.txt')), reply_markup=button, reply_markup=button)
+    await message.reply_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
     
 button = reply_markup=InlineKeyboardMarkup(
-[[InlineKeyboardButton("🇦🇿 Şəhid", callback_data="deyis")]])
-
-button = reply_markup=InlineKeyboardMarkup(
-[[InlineKeyboardButton("🔐 Bağla", callback_data="close")]])
+[[InlineKeyboardButton("🇦🇿 Şəhid", callback_data="deyis"),
+InlineKeyboardButton("🔐 Bağla", callback_data="close")]])
 
 
 @app.on_callback_query(filters.regex("deyis"))
 async def deyis(_, query: CallbackQuery):
-    await query.edit_message_text((await random_line('Sehid/sehid.txt')), reply_markup=button, reply_markup=button)
+    await query.edit_message_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
