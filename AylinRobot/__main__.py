8d728727958f3b14c7e013e24b2b030451bc6ac4
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-18 14:19:55 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -53,6 +53,15 @@ async def ghelp(client, message):
         reply_markup=Button.GHELP_BUTTONS
     )          
     
+
+@app.on_message(filters.private & filters.incoming & filters.command(['help']))
+async def ghelp(client, message):
+    await message.reply_photo(
+        AylinIMG,
+        caption=Translation.GHELP_TEXT.format(message.from_user.mention, Config.BOT_USERNAME, message.chat.title, Config.OWNER_NAME),
+        reply_markup=Button.GHELP_BUTTONS
+    )          
+
     
     
 app.start()
