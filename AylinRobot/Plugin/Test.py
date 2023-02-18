ffa90de6e0699db1f0b1b120769b1261f4ce347a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-18 09:01:38 +0400

    update Test.py

--- AylinRobot/Plugin/Test.py
+++ AylinRobot/Plugin/Test.py
@@ -1,1 +1,15 @@
+from AylinRobot import AylinRobot as app
+from pyrogram import Client, emoji, filters
 
+TARGET = -1001845751616
+# Welcome message template
+MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.org/)'s group chat {}!"
+
+@app.on_message(filters.chat(TARGET) & filters.new_chat_members)
+async def welcome(client, message):
+    # Build the new members list (with mentions) by using their first_name
+    new_members = [u.mention for u in message.new_chat_members]
+    # Build the welcome message by using an emoji and the list we built above
+    text = MESSAGE.format(emoji.SPARKLES, ", ".join(new_members))
+    # Send the welcome message, without the web page preview
+    await message.reply_text(text, disable_web_page_preview=True)
