8535696819c8725e9cb4bda8b8116fabeef02312
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-18 16:15:30 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -38,12 +38,8 @@ async def start(client, message):
     
 @app.on_message(filters.group & filters.incoming & filters.command(['start']))
 async def gstart(client, message):
-    await message.reply_photo(
-        AylinIMG,
         caption=Translation.GSTART_TEXT.format(message.from_user.mention, Config.BOT_USERNAME, message.chat.title, Config.OWNER_NAME),
         reply_markup=Button.BAGLA_BUTTONS
-    )
-    
     
 @app.on_message(filters.group & filters.incoming & filters.command(['help']))
 async def ghelp(client, message):
