edc5107cf19512cb2affedccac37bc803301d6a6
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-19 13:31:09 +0400

    update Song🎧.py

--- AylinRobot/Music/Song🎧.py
+++ AylinRobot/Music/Song🎧.py
@@ -97,9 +97,7 @@ def song(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-         m.edit( = "📤 Göndərilir...",
-         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("📤 Göndərilir...", callback_data="progress")]]))
+        m.edit("📤 Yüklənir..")
         message.reply_audio(audio_file, caption=caption_for_private, quote=False, title=title, duration=dur, thumb=thumb_name, performer = f"{Config.PLAYLIST_NAME}", reply_markup=buttons['markup_for_private'])
         m.delete()
         app.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=caption_for_logchannel, performer = f"{Config.BOT_USERNAME}", title=title, duration=dur, thumb=thumb_name, reply_markup=buttons['add_to_group'])
