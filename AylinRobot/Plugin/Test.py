29b6512e500c286a139ca8debb4334b2e907a999
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-19 16:08:32 +0400

    update Test.py

--- AylinRobot/Plugin/Test.py
+++ AylinRobot/Plugin/Test.py
@@ -66,7 +66,7 @@ def song(client, message):
         return
     m.edit("Ssss")
     try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
