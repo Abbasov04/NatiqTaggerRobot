bf51d3f7f316f8bac87e8705a7b477b9d82beae3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 12:15:40 +0400

    update config.py

--- AylinRobot/config.py
+++ AylinRobot/config.py
@@ -28,5 +28,5 @@ class Config:
    ALIVE_NAME = os.environ.get("ALIVE_NAME", "HUSEYN")
    CHANNEL = os.environ.get("CHANNEL", "HuseynH")
    SUPPORT = os.environ.get("SUPPORT", "HuseynH")
-   ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/f6c186e3c581a223856c4.mp4") 
+   ALIVE_IMG = os.environ.get("ALIVE_IMG", "https://telegra.ph/file/425d1e4790954b3d29e87.mp4") 
    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/b2c2ed59a89933a384ae3.jpg")
