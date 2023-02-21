bc6786a60f586599d14f5b43e1ba174fb18b130f
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 17:32:54 +0400

    update tag.py

--- Telethon/Plugin/tag.py
+++ Telethon/Plugin/tag.py
@@ -16,9 +16,6 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
  
-api_id = Config.API_ID
-api_hash = Config.API_HASH
-bot_token = Config.BOT_TOKEN
 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
