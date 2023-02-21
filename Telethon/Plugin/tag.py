73eb20a9d8a099577e6ece2fd9a965a3df53d754
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 17:28:16 +0400

    update tag.py

--- Telethon/Plugin/tag.py
+++ Telethon/Plugin/tag.py
@@ -9,7 +9,19 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from AylinRobot.config import Config
 from Telethon.Mesajlar import heyvan, sehidler, soz, mafia, emoji, seher, bayrag
 
-from Telethon.Plugin.Pin import Pin
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+ 
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN
+
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
 
 anlik_calisan = []
  
@@ -21,17 +33,7 @@ rxyzdev_tagTot = {}
 rxyzdev_initT = {}
  
  
- logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s'
-)
-LOGGER = logging.getLogger(__name__)
  
-api_id = Config.API_ID
-api_hash = Config.API_HASH
-bot_token = Config.BOT_TOKEN
-
-client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 	
 @client.on(events.NewMessage(pattern='^.cancel ?(.*)'))
