e4734dedae6f8cc91f2209f6f28442d620a893ce
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 13:12:15 +0400

    update Chatbot🫂.py

--- AylinRobot/Plugin/Chatbot🫂.py
+++ AylinRobot/Plugin/Chatbot🫂.py
@@ -17,46 +17,46 @@ active_chats = []
 async def chatbot_status(_, message):
     global active_chats
     if len(message.command) != 2:
-        await message.reply_Aylin("/chatbot [ON] və yaxud [OFF] yazmadınız")
+        await message.reply_text("/chatbot [ON] və yaxud [OFF] yazmadınız")
         return
-    status = message.Aylin.split(None, 1)[1]
+    status = message.text.split(None, 1)[1]
     chat_id = message.chat.id
     
     if status == "ON" or status == "on" or status == "On":
         if chat_id not in active_chats:
             active_chats.append(chat_id)
             text = "✅ **ChatBot bu qrupda aktiv olundu !**"
-            await message.reply_Aylin(Aylin)
+            await message.reply_text(text)
             return
-        await message.reply_Aylin("⚠️ **ChatBot onsuzda aktivdir !**")
+        await message.reply_text("⚠️ **ChatBot onsuzda aktivdir !**")
         return
     elif status == "OFF" or status == "off" or status == "Off":
         if chat_id in active_chats:
             active_chats.remove(chat_id)
-            await message.reply_Aylin("⛔️ **ChatBot bu qrupda deaktiv olundu !**")
+            await message.reply_text("⛔️ **ChatBot bu qrupda deaktiv olundu !**")
             return
-        await message.reply_Aylin("⚠️ **ChatBot onsuzda deaktivdir !**")
+        await message.reply_text("⚠️ **ChatBot onsuzda deaktivdir !**")
         return
     
     else:
-        await message.reply_Aylin("/chatbot [ON] və yaxud [OFF] yazmadınız")
+        await message.reply_text("/chatbot [ON] və yaxud [OFF] yazmadınız")
         
-@app.on_message(filters.Aylin)
+@app.on_message(filters.text)
 async def start(_, msg: Message):
     global active_chats        
-    Aylin = msg.Aylin.lower()
+    text = msg.text.lower()
     chat_id = msg.chat.id
     if msg.chat.id not in active_chats:
         return
-    if "salam" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(salam)}")
-    if "necəsən" in text or "necesen" in Aylin or "netersen" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(necesen)}")
-    if "sagol" in Aylin or "sağol" in Aylin or "saqol" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(sagol)}")
-    if "getdim" in Aylin or "gedim" in Aylin or "gedirəm" in Aylin or "gedirem" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(getdim)}")
-    if "geldim" in Aylin or "gəldim" in Aylin or "gəl" in Aylin or "gəlirəm" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(geldim)}")
-    if "ban" in Aylin or "mute" in Aylin or "purge" in text or "gban" in Aylin:
-        await msg.reply_Aylin(f"{random.choice(ban)}")
+    if "salam" in text:
+        await msg.reply_text(f"{random.choice(salam)}")
+    if "necəsən" in text or "necesen" in text or "netersen" in text:
+        await msg.reply_text(f"{random.choice(necesen)}")
+    if "sagol" in text or "sağol" in text or "saqol" in text:
+        await msg.reply_text(f"{random.choice(sagol)}")
+    if "getdim" in text or "gedim" in text or "gedirəm" in text or "gedirem" in text:
+        await msg.reply_text(f"{random.choice(getdim)}")
+    if "geldim" in text or "gəldim" in text or "gəl" in text or "gəlirəm" in text:
+        await msg.reply_text(f"{random.choice(geldim)}")
+    if "ban" in text or "mute" in text or "purge" in text or "gban" in text:
+        await msg.reply_text(f"{random.choice(ban)}")
