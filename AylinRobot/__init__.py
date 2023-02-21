808d0aa69758afbb37f90b90945455a4f4675752
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 19:17:20 +0400

    update __init__.py

--- AylinRobot/__init__.py
+++ AylinRobot/__init__.py
@@ -19,3 +19,39 @@ AylinRobot = Client(
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
 )
+
+
+from telethon import TelegramClient
+
+from Telethon.Mesajlar import * 
+
+from logging import *
+from AylinRobot.config import Config
+
+TOKEN    = Config.TOKEN
+API_ID   = Config.API_ID
+API_HASH = Config.API_HASH
+
+basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=INFO)
+
+logger = getLogger(__name__)
+
+_ = TelegramClient('Aylin', api_id=API_ID, api_hash=API_HASH)
+bot = _.start(bot_token=TOKEN)
+
+owner = config.OWNER
+
+anlıq = []
+
+tək = []
+
+qruplar = []
+
+istifadeci = []
+
+admins = []
+
+delay = Config.DELAY
+
+botadı = Config.BOT_USERNAME
+
