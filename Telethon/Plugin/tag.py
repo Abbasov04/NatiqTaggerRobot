175c142d84118be96f9466ae6eb28b69b58075b5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 16:24:13 +0400

    update tag.py

--- Telethon/Plugin/tag.py
+++ Telethon/Plugin/tag.py
@@ -7,7 +7,7 @@ from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 
 from AylinRobot.config import Config
-from Mesajlar.tag import heyvan, sehidler, soz, mafia, emoji, seher, bayrag
+from Telethon.Mesajlar.tag import heyvan, sehidler, soz, mafia, emoji, seher, bayrag
 
 logging.basicConfig(
     level=logging.INFO,
