5b344c7fad9ff33af1ab36950eb7d631ad5c5bad
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 18:54:03 +0400

    update Pin.py

--- Telethon/Plugin/Pin.py
+++ Telethon/Plugin/Pin.py
@@ -2,24 +2,34 @@ import logging, asyncio, random
 from telethon import events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
+
 from AylinRobot.config import Config
 
 
+
+
+
+
+SAHIB = Config.OWNER_ID
+BOT_NAME = Config.BOT_NAME
+
 @client.on(events.NewMessage(pattern="^.pin ?(.*)"))
 async def pin(event):
+    if event.sender_id == SAHIB:
         if not event.reply_to_msg_id:
             return await event.reply("🗨 Zəhmət Olmasa Bir Mesaja Yanıt Verin")
         await event.reply("📌 Sahibim Mesajınlz Pinləndi!")
         await event.client.pin_message(event.chat_id, event.reply_to_msg_id, notify=True)
     else:
-        await event.reply(f"Sən {Config.BOT_NAME} Bota Sahib Deyilsən!\n⛔ Pinləməyə Çalışma.")
+        await event.reply(f"Sən {BOT_NAME} Bota Sahib Deyilsən!\n⛔ Pinləməyə Çalışma.")
  
 
 @client.on(events.NewMessage(pattern="^.unpin ?(.*)"))
 async def unpin(event):
+    if event.sender_id == SAHIB:
         if not event.reply_to_msg_id:
             return await event.reply("🗨 Zəhmət Olmasa Pinlənmiş Mesaja Yanıt Verin")
         await event.reply("Sahibim Pinlənmiş Mesaj Qaldırıldı")
         await event.client.unpin_message(event.chat_id)
     else:
-        await event.reply(f"Sən {Config.BOT_NAME} Bota Sahib Deyilsən!\n⛔ UnPinləməyə Çalışma.")
+        await event.reply(f"Sən {BOT_NAME} Bota Sahib Deyilsən!\n⛔ UnPinləməyə Çalışma.")
