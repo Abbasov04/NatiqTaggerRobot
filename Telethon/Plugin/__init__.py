373c1076befacf4a3ef51bb9548b96ba8d2d71a5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-21 17:14:50 +0400

    update __init__.py

--- Telethon/Plugin/__init__.py
+++ Telethon/Plugin/__init__.py
@@ -9,3 +9,16 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+ 
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN
+
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
