7105aa5408014a2544bd08c3ffd77116e0d78b93
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-22 20:42:00 +0400

    update Telethon.py

--- Telethon/Plugin/Telethon.py
+++ Telethon/Plugin/Telethon.py
@@ -552,10 +552,9 @@ async def handler(event):
 
 @client.on(events.ChatAction)
 async def handler(event):
-  ad = f"[{usr.first_name}](tg://user?id={usr.id})  
     if event.user_left:
         await event.reply("Əla Birdə gəlmə")
-
+  ad = f"[{usr.first_name}](tg://user?id={usr.id})  
 userjoin = (
 
     f"{ad}Xoş Gəldoin",
