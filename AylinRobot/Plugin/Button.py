bbff902330336aaa5fb2b108c45554ecfdd16114
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-22 14:35:16 +0400

    update Button.py

--- AylinRobot/Plugin/Button.py
+++ AylinRobot/Plugin/Button.py
@@ -67,3 +67,5 @@ InlineKeyboardButton('↪️ Geri Qayıt', callback_data='help'),]])
 BH_BUTTONS = InlineKeyboardMarkup(
 [[InlineKeyboardButton('🔐 Bağla', callback_data='close'),   
 InlineKeyboardButton('↪️ Geri Qayıt', callback_data='home'),]])
+GS_BUTTONS = InlineKeyboardMarkup(
+[[InlineKeyboardButton('🔐 Bağla', callback_data='close'),   
