67709e15fc2f45e63f86c2d435dcfd31cabb02a5
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 22:37:39 +0400

    update Sozoyunu.py

--- AylinRobot/Oyunlar/Sozoyunu.py
+++ AylinRobot/Oyunlar/Sozoyunu.py
@@ -49,3 +49,55 @@ async def kelimeoyun(c:Client, m:Message):
 ✏️ Qarışıq Həriflərdən düzgün sözü tapın.
         """
         await c.send_message(m.chat.id, text)
+        
+        
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from pyrogram.types import Message
+from helpers.keyboards import *
+from helpers.kelimeler import kelime_sec
+
+
+
+@app.on_message(filters.command("kec") & ~filters.private & ~filters.channel)
+async def passs(c:Client, m:Message):
+    global oyun
+    
+    try:
+        aktif = oyun[m.chat.id]["aktif"]
+        aktif = True
+    except:
+        aktif = False
+
+    if aktif:
+        if oyun[m.chat.id]["kec"] < 3:
+            oyun[m.chat.id]["kec"] += 1 
+            await c.send_message(m.chat.id,f"😑 Maksimum 3 keçmə haqqınız var!\n➡️ Söz uğurla keçildi !\n✏️ Düzgün Söz : **<code>{oyun[m.chat.id]['kelime']}</code>**")
+            
+            oyun[m.chat.id]["kelime"] = kelime_sec()
+            oyun[m.chat.id]["aktif"] = True
+            
+            kelime_list = ""
+            kelime = list(oyun[m.chat.id]['kelime'])
+            shuffle(kelime)
+            
+            for harf in kelime:
+                kelime_list+= harf + " "
+            
+            text = f"""
+🎯 Raund : {oyun[m.chat.id]['round']}/20 
+📝 Tapılacaq Sözlər :   <code>{kelime_list}</code>
+💰 Qazandığın Xal : 1
+🔎 İlk hərf : 1. {oyun[m.chat.id]["kelime"][0]}
+✍🏻 𝖴𝗓𝗎𝗇𝗅uq: {int(len(kelime_list)/2)} 
+
+✏️ Qarışıq həriflərdən düzgün sözü tapın.
+            """
+            await c.send_message(m.chat.id, text)
+            
+        else:
+            await c.send_message(m.chat.id, f"<code>**❗ Keçiş Düzgün Qeydedildi! </code> \n Oyunu dayandırmaq üçün yazıb /cancel dayandıra bilərsiniz ✍🏻**")
+    else:
+        await m.reply(f"❗ **Qrupunuzda aktiv oyun oynanılır!\n Yeni bir oyuna başlamaq üçün /oyun yazabilərsiniz✍🏻**")
+        
