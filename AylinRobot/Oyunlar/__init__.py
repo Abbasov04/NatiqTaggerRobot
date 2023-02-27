96d474fff876a5b2313bcc81a9e6d431a9c70485
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 15:43:36 +0400

    update __init__.py

--- AylinRobot/Oyunlar/__init__.py
+++ AylinRobot/Oyunlar/__init__.py
@@ -10,3 +10,9 @@ __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
 
+
+# Oyun Verileri
+oyun = {}  # type: ignore
+
+# rating
+rating = {}  # type: ignore
