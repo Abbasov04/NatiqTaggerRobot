35ebe787d74f03e0a0555996ee5969e8e7275fa3
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 16:41:37 +0400

    update Sozoyunu.py

--- AylinRobot/Oyunlar/Sozoyunu.py
+++ AylinRobot/Oyunlar/Sozoyunu.py
@@ -221,3 +221,56 @@ async def ratingsa(c:Client, m:Message):
                     break
                 
     await c.send_message(m.chat.id, metin)
+
+
+
+
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from pyrogram.types import Message
+from helpers.keyboards import *
+from helpers.kelimeler import kelime_sec
+
+
+
+@app.on_message(filters.command("kec") & ~filters.private & ~filters.channel)
+async def passs(c:Client, m:Message):
+    global oyun
+    
+    try:
+        aktif = oyun[m.chat.id]["aktif"]
+        aktif = True
+    except:
+        aktif = False
+
+    if aktif:
+        if oyun[m.chat.id]["kec"] < 30:
+            oyun[m.chat.id]["kec"] += 1
+            await c.send_message(m.chat.id,f"❗ Sizin Tam Yol Haqqınız Var!\n➡️ Növbəti Sözə Keçdim !\n✏️ Doğru söz : **<code>{oyun[m.chat.id]['kelime']}</code>**")
+            
+            oyun[m.chat.id]["kelime"] = kelime_sec()
+            oyun[m.chat.id]["aktif"] = True
+            
+            kelime_list = ""
+            kelime = list(oyun[m.chat.id]['kelime'])
+            shuffle(kelime)
+            
+            for harf in kelime:
+                kelime_list+= harf + " "
+            
+            text = f"""
+🎯 Raund : {oyun[m.chat.id]['round']}/100 
+🔡 Söz :   <code>{kelime_list}</code>
+🏆 Qazandığın Xal : 50
+ℹ İ𝗉𝗎𝖼𝗎 : 1. {oyun[m.chat.id]["kelime"][0]}
+〽️ 𝖴𝗓𝗎𝗇𝗅uq: {int(len(kelime_list)/2)} 
+
+✏️ Qarışıq hərflərdən ibarət sözü tapın 
+            """
+            await c.send_message(m.chat.id, text)
+            
+        else:
+            await c.send_message(m.chat.id, f"<code>**❗ Keçid Düzgün Saxlanıldı! </code> \n Oyunu dayandırmaq üçün  /dayan yaza bilərsiniz ✍🏻**")
+    else:
+        await m.reply(f"❗ **Qrupumuzda Hal-Hazırda Aktiv Oyun Yoxdur!\n Yeni Oyuna Başlamaq Üçün /oyna Əmrindən İsdifadə Edin ✍🏻**")
