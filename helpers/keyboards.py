5f525ecf02833e9ed23bc3de1f385bb7ea294d0b
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 15:49:00 +0400

    Add files via upload

---
+++ helpers/keyboards.py
@@ -0,0 +1,8 @@
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+
+kanal = InlineKeyboardMarkup([
+    [InlineKeyboardButton("🇦🇿 𝐎 𝐖 𝐍 𝐄 𝐑 👨‍💻" , url= "t.me/sesizKOLGE")]
+])
+
+
