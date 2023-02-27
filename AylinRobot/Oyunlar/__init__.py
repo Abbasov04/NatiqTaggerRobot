64f2d4c3662a014675dc0ad2d6fa2c2d06fa6256
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 18:22:15 +0400

    update __init__.py

--- AylinRobot/Oyunlar/__init__.py
+++ AylinRobot/Oyunlar/__init__.py
@@ -4,6 +4,21 @@
 from AylinRobot.config import Config
 from os.path import dirname, basename, isfile, join
 import glob
+from time import sleep
+from pyrogram import Client
+import logging
+from dotenv import load_dotenv, set_key, unset_key
+from os import getenv
+
+load_dotenv('config.env')
+
+# THE LOGGING
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
