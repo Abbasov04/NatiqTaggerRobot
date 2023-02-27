dca1a1c8815a0cdfe83a5be240d83202ff1a1a4a
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 16:04:15 +0400

    update Sozoyunu.py

--- AylinRobot/Oyunlar/Sozoyunu.py
+++ AylinRobot/Oyunlar/Sozoyunu.py
@@ -49,3 +49,35 @@ async def kelimeoyun(c:Client, m:Message):
         """
         await c.send_message(m.chat.id, text)
         
+
+
+
+
+
+
+
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from kelime_bot import USERNAME
+from pyrogram.types import Message
+from kelime_bot.helpers.keyboards import *
+from helpers.kelimeler import kelime_sec
+
+
+
+@app.on_message(filters.command("stop") & ~filters.private & ~filters.channel)
+async def stop(c:Client, m:Message):
+    global oyun
+    
+    siralama = []
+    for i in oyun[m.chat.id]["oyuncular"]:
+        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} Bal")
+    siralama.sort(reverse=True)
+    siralama_text = ""
+    for i in siralama:
+        siralama_text += i + "\n"     
+    
+    await c.send_message(m.chat.id, f"**👤 {m.from_user.mention}** Tərəfindən Oyun Sonlandırıldı\n\nYeni Oyuna Başlamaq Üçün /oyna Əmrindən İsdifadə Edin !\n\n 🏆 Xallar səyfəsi  :\n\n{siralama_text}")
+    oyun[m.chat.id] = {}
+    
