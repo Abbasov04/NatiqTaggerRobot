514657728c7e61bf5190eb727a04fe1ea5a087e4
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 19:23:55 +0400

    update Sozoyunu.py

--- AylinRobot/Oyunlar/Sozoyunu.py
+++ AylinRobot/Oyunlar/Sozoyunu.py
@@ -103,7 +103,7 @@ from helpers.keyboards import *
 from helpers.kelimeler import kelime_sec
 
 
-@app.on_message(filters.text & ~filters.private & ~filters.channel)
+@app.on_message(filters.text & ~filters.group)
 async def buldu(c:Client, m:Message):
     global oyun
     global rating
@@ -158,7 +158,47 @@ async def buldu(c:Client, m:Message):
     except KeyError:
         pass
             
+gonderilmedi = True
+data_message = None
+EKLENEN_CHATS = []
+@app.on_message()
+async def data(c:Client, m:Message):
+    global EKLENEN_CHATS
+    global gonderilmedi
+    global data_message
+    
+    chat_id = str(m.chat.id)
+    
+    if chat_id in EKLENEN_CHATS:
+        return
+
+    if gonderilmedi:
+        data_message= await c.send_message(OWNER_ID, f"{OWNER_ID}")
+        gonderilmedi = False
+        
+    
+    else:
+        chats = await c.get_messages(OWNER_ID, data_message.message_id)
+        chats = chats.text.split()
+        
+        if chat_id in chats:
+            pass
+        else:
+            chats.append(chat_id)
+            EKLENEN_CHATS.append(chat_id)
+            data_text = ""
+            for i in chats:
+                data_text += i + " "
+            await c.edit_message_text(OWNER_ID, data_message.message_id, data_text)
+            
+            
           
+
+
+
+
+
+
 
 from AylinRobot.Oyunlar import rating
 from pyrogram import Client
