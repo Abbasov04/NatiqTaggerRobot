8ef70594615e8e5e86def1d9df7eaeb18ffec935
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 22:53:30 +0400

    update Sozoyunu.py

--- AylinRobot/Oyunlar/Sozoyunu.py
+++ AylinRobot/Oyunlar/Sozoyunu.py
@@ -97,7 +97,183 @@ async def passs(c:Client, m:Message):
             await c.send_message(m.chat.id, text)
             
         else:
-            await c.send_message(m.chat.id, f"<code>**❗ Keçiş Düzgün Qeydedildi! </code> \n Oyunu dayandırmaq üçün yazıb /cancel dayandıra bilərsiniz ✍🏻**")
+            await c.send_message(m.chat.id, f"<code>**❗ Keçiş Düzgün Qeydedildi! </code> \n Oyunu dayandırmaq üçün yazıb /stop dayandıra bilərsiniz ✍🏻**")
     else:
-        await m.reply(f"❗ **Qrupunuzda aktiv oyun oynanılır!\n Yeni bir oyuna başlamaq üçün /oyun yazabilərsiniz✍🏻**")
+        await m.reply(f"❗ **Qrupunuzda aktiv oyun oynanılır!\n Yeni bir oyuna başlamaq üçün /game yazabilərsiniz✍🏻**")
+        
+        
+        
+        
+from AylinRobot.Oyunlar import rating
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+
+
+@app.on_message(filters.command("skor"))
+async def ratingsa(c:Client, m:Message):
+    global rating
+    metin = """📝 Küresel Grup Derecelendirmesi :
+
+"""
+    eklenen = 0
+    puanlar = []
+    for kisi in rating:
+        puanlar.append(rating[kisi])
+    puanlar.sort(reverse = True)
+    for puan in puanlar:
+        for kisi in rating:
+            if puan == rating[kisi]:
+                metin += f"**{kisi}** : {puan}  Puan\n"
+                eklenen += 1
+                if eklenen == 20:
+                    break
+                
+    await c.send_message(m.chat.id, metin)
+        
+        
+from AylinRobot.Oyunlar import oyun, rating
+from pyrogram import Client, filters
+
+
+@app.on_message(filters.command("data") & filters.user("HuseynH")) 
+async def data(c:Client, m):
+    await m.reply(oyun)
+    await m.reply(rating)
+        
+        
+        
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from pyrogram.types import Message
+from helpers.keyboards import *
+from helpers.kelimeler import kelime_sec
+
+
+
+@app.on_message(filters.command("stop") & ~filters.private & ~filters.channel)
+async def stop(c:Client, m:Message):
+    global oyun
+    
+    siralama = []
+    for i in oyun[m.chat.id]["oyuncular"]:
+        siralama.append(f"{i}   :   {oyun[m.chat.id]['oyuncular'][i]} Bal")
+    siralama.sort(reverse=True)
+    siralama_text = ""
+    for i in siralama:
+        siralama_text += i + "\n"     
+    
+    await c.send_message(m.chat.id, f"**{m.from_user.mention}** Tərəfindən Oyun Dayandırıldı \n\nYeni Oyuna Başlamaq üçün/oyun Yaza Bilərsiniz\n\n 📝 Yığdığınız Xal  :\n\n{siralama_text}")
+    oyun[m.chat.id] = {}
+    
+        
+        
+from pyrogram import Client
+from pyrogram import filters
+from random import shuffle
+from pyrogram.types import Message
+from AylinRobot.Oyunlar import rating
+from helpers.keyboards import *
+from kelime_bot.helpers.kelimeler import kelime_sec
+from kelime_bot import *
+
+
+
+
+
+
+
+
+
+@app.on_message(filters.text & ~filters.private & ~filters.channel)
+async def buldu(c:Client, m:Message):
+    global oyun
+    global rating
+    try:
+        if m.chat.id in oyun:
+            if m.text.lower() == oyun[m.chat.id]["kelime"]:
+                await c.send_message(m.chat.id,f"✨ Təbriklər !\n**{m.from_user.mention}** \n**<code>{oyun[m.chat.id]['kelime']}</code>** , Sözünü Tapdı 🤩")
+                if f"{m.from_user.mention}" in rating:
+                    rating[f"{m.from_user.mention}"] += 1
+                else:
+                    rating[f"{m.from_user.mention}"] = 1
+                
+                try:
+                    puan = oyun[m.chat.id]["oyuncular"][str(m.from_user.mention)]
+                    oyun[m.chat.id]["oyuncular"][str(m.from_user.mention)] +=1
+                except KeyError:
+                    oyun[m.chat.id]["oyuncular"][str(m.from_user.mention)] = 1
+                
+                
+                oyun[m.chat.id]["kelime"] = kelime_sec()
+                oyun[m.chat.id]["round"] = oyun[m.chat.id]["round"] + 1
+                
+                if not oyun[m.chat.id]["round"] <= 20:
+                    siralama = []
+                    for i in oyun[m.chat.id]["oyuncular"]:
+                        siralama.append(f"{i} :   {oyun[m.chat.id]['oyuncular'][i]}  Xal")
+                    siralama.sort(reverse=True)
+                    siralama_text = ""
+                    for i in siralama:
+                        siralama_text += i + "\n"
+                    
+                    return await c.send_message(m.chat.id,f"✅ Oyun Qutardı✓ \n\n📝 Qazandığı Xal :\n\n{siralama_text}\n\n Yeni Oyuna Başlamaq üçün /oyun Yaza Bilərsən !")
+                
+                
+                
+                kelime_list = ""
+                kelime = list(oyun[m.chat.id]['kelime'])
+                shuffle(kelime)
+                for harf in kelime:
+                    kelime_list+= harf + " "
+            
+                text = f"""
+🎯 Raund : {oyun[m.chat.id]['round']}/20
+📝 Tapılacaq Söz :   <code>{kelime_list}</code>
+💰 Yığdığınız Xal: 1
+🔎 İlk hərf: 1. {oyun[m.chat.id]["kelime"][0]}
+✍🏻 Uzunluq : {int(len(kelime_list)/2)} 
+
+✏️ Qarışıq həriflərdən düzgün sözü tapın.
+                        """
+                await c.send_message(m.chat.id, text)
+    except KeyError:
+        pass
+    
+    
+gonderilmedi = True
+data_message = None
+EKLENEN_CHATS = []
+@Client.on_message()
+async def data(c:Client, m:Message):
+    global EKLENEN_CHATS
+    global gonderilmedi
+    global data_message
+    
+    chat_id = str(m.chat.id)
+    
+    if chat_id in EKLENEN_CHATS:
+        return
+
+    if gonderilmedi:
+        data_message= await c.send_message(OWNER_ID, f"{OWNER_ID}")
+        gonderilmedi = False
+        
+    
+    else:
+        chats = await c.get_messages(OWNER_ID, data_message.message_id)
+        chats = chats.text.split()
+        
+        if chat_id in chats:
+            pass
+        else:
+            chats.append(chat_id)
+            EKLENEN_CHATS.append(chat_id)
+            data_text = ""
+            for i in chats:
+                data_text += i + " "
+            await c.edit_message_text(OWNER_ID, data_message.message_id, data_text)
+            
+            
         
