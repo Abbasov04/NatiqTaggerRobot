d947b45319f366c3f93d2eb6863605799bc145d9
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 22:15:45 +0400

    update __init__.py

--- AylinRobot/Oyunlar/__init__.py
+++ AylinRobot/Oyunlar/__init__.py
@@ -4,23 +4,11 @@
 
 from os.path import dirname, basename, isfile, join
 import glob
-from time import sleep
-from pyrogram import Client
-import logging
-
-# THE LOGGING
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-LOGGER = logging.getLogger(__name__)
-
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
-
 
 # Oyun Verileri
 oyun = {}  # type: ignore
