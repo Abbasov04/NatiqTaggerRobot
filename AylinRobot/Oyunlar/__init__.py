1f602b2e4995f3f337f560e0f4fa89cc551063cb
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 18:26:55 +0400

    update __init__.py

--- AylinRobot/Oyunlar/__init__.py
+++ AylinRobot/Oyunlar/__init__.py
@@ -1,16 +1,12 @@
 # @AylinRobot
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
-from AylinRobot.config import Config
+
 from os.path import dirname, basename, isfile, join
 import glob
 from time import sleep
 from pyrogram import Client
 import logging
-from dotenv import load_dotenv, set_key, unset_key
-from os import getenv
-
-load_dotenv('config.env')
 
 # THE LOGGING
 logging.basicConfig(
@@ -25,10 +21,10 @@ __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
 
-if Config.OWNER_ID and len(Config.OWNER_ID) and Config.OWNER_ID.isdigit():
-    Config.OWNER_ID = int(Config.OWNER_ID)  # type: ignore
+if OWNER_ID and len(OWNER_ID) and OWNER_ID.isdigit():
+    OWNER_ID = int(OWNER_ID)  # type: ignore
 else:
-    Config.OWNER_ID = None  # type: ignore
+    OWNER_ID = None  # type: ignore
 
 
 # Oyun Verileri
