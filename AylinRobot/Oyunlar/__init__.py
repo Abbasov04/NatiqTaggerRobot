41d9e1fb7bbf410367b95c2a09181c1543f1ffce
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-02-27 18:20:28 +0400

    update __init__.py

--- AylinRobot/Oyunlar/__init__.py
+++ AylinRobot/Oyunlar/__init__.py
@@ -1,7 +1,7 @@
 # @AylinRobot
 # Sahib @HuseynH
 # Repo Açığdısa İcazəsis Götürmə Oğlum
-
+from AylinRobot.config import Config
 from os.path import dirname, basename, isfile, join
 import glob
 
@@ -9,6 +9,12 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
     basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
 ]
+
+if Config.OWNER_ID and len(Config.OWNER_ID) and Config.OWNER_ID.isdigit():
+    Config.OWNER_ID = int(Config.OWNER_ID)  # type: ignore
+else:
+    Config.OWNER_ID = None  # type: ignore
+
 
 # Oyun Verileri
 oyun = {}  # type: ignore
