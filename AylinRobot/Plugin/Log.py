db979df8f7ad44b1f217064c20c5ce1d74e238de
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2023-04-15 15:48:02 +0400

    update Log.py

--- AylinRobot/Plugin/Log.py
+++ AylinRobot/Plugin/Log.py
@@ -1,7 +1,7 @@
 from AylinRobot import AylinRobot as app
 from pyrogram.errors.exceptions import ChatAdminRequired
 from pyrogram.types import Photo, Video, Animation, Audio
-from Plugin.filters import command
+from Log.filters import command
 
 
 CHANNEL_ID = -1001872847436
