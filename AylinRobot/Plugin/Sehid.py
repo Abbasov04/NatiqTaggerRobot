f99b9293d8e0f188fabe81480a6a9c0d70282cd7
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2023-04-16 22:44:58 +0400

    update Sehid.py

--- AylinRobot/Plugin/Sehid.py
+++ AylinRobot/Plugin/Sehid.py
@@ -1,27 +1,26 @@
-# @AylinRobot
-# Sahib @HuseynH
-# Repo Açığdısa İcazəsis Götürmə Oğlum
-
+from config import Config
 import secrets
 import string
 import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from AylinRobot.config import Config
+from cryptography.fernet import Fernet
+from AylinRobot import AylinRobot as app
+
 
 button = InlineKeyboardMarkup([
     [InlineKeyboardButton("🔄Dəyiş", callback_data="deyis")]
 ])
 
-@client.on_message(filters.command("sehid") & ~filters.edited)
+@bot.on_message(filters.command("sehid") & ~filters.edited)
 async def commit(_, message):
-    await message.reply_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
+    await message.reply_text((await random_line('AykhanPro/txt/sehid.txt')), reply_markup=button)
 
 
 
-@client.on_callback_query(filters.regex("deyis"))
+@bot.on_callback_query(filters.regex("deyis"))
 async def deyis(_, query: CallbackQuery):
-    await query.edit_message_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
+    await query.edit_message_text((await random_line('AykhanPro/txt/sehid.txt')), reply_markup=button)
 
 
 bot.run()
