727a074ef5cb0c23996c98a981a6c5e0d9d3fb42
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2023-04-16 22:37:11 +0400

    update Sehid.py

--- AylinRobot/Plugin/Sehid.py
+++ AylinRobot/Plugin/Sehid.py
@@ -13,13 +13,13 @@ button = InlineKeyboardMarkup([
     [InlineKeyboardButton("🔄Dəyiş", callback_data="deyis")]
 ])
 
-@bot.on_message(filters.command("sehid") & ~filters.edited)
+@client.on_message(filters.command("sehid") & ~filters.edited)
 async def commit(_, message):
     await message.reply_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
 
 
 
-@bot.on_callback_query(filters.regex("deyis"))
+@client.on_callback_query(filters.regex("deyis"))
 async def deyis(_, query: CallbackQuery):
     await query.edit_message_text((await random_line('Sehid/sehid.txt')), reply_markup=button)
 
