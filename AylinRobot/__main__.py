40d16723ac3ac15296e9442a8b7562a130a559b8
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2023-10-19 15:05:05 +0400

    update __main__.py

--- AylinRobot/__main__.py
+++ AylinRobot/__main__.py
@@ -10,7 +10,7 @@ import string
 import asyncio
 from AylinRobot.config import Config
 from asyncio import TimeoutError
-from AylinRobot.translation import Translation
+from Telethon.Plugin import Telethon
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
@@ -22,25 +22,6 @@ from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from AylinRobot import AylinRobot as app
 from AylinRobot import LOGGER
-
-AylinIMG = f"{Config.START_IMG}"
-
-@app.on_message(filters.private & filters.incoming & filters.command(['start']))
-async def start(client, message):
-    await message.reply_photo(
-        AylinIMG,
-        caption=Translation.START_TEXT.format(message.from_user.mention, Config.BOT_USERNAME, Config.OWNER_NAME, Config.BOT_NAME),
-        reply_markup=Button.START_BUTTONS
-    )
-    
-    
-@app.on_message(filters.group & filters.incoming & filters.command(['start']))
-async def start(client, message):
-    await message.reply_photo(
-        AylinIMG,
-        caption=Translation.GSTART_TEXT.format(message.from_user.mention, Config.BOT_USERNAME,  Config.BOT_NAME, message.chat.title),
-        reply_markup=Button.GS_BUTTONS
-    )    
     
     
 app.start()
